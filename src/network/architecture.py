9e511f42e37a06f0e5b1389d64223517ca1ed557
Arthur Flôr <arthurflor23@gmail.com>
2019-10-15 22:15:45 -0300

    Improve model with GRU Update requirements Upload `similar_error_analysis` function

--- src/network/architecture.py
+++ src/network/architecture.py
@@ -1,11 +1,11 @@
 """Networks to the Handwritten Text Recognition Model"""
 
-from tensorflow.keras.layers import Input, Conv2D, Bidirectional, LSTM, Dense
-from tensorflow.keras.layers import Dropout, BatchNormalization, MaxPooling2D
-from tensorflow.keras.layers import Reshape, Activation, LeakyReLU, PReLU
-from tensorflow.keras.constraints import MaxNorm
-from tensorflow.keras.optimizers import RMSprop
 from network.layers import FullGatedConv2D, GatedConv2D
+from tensorflow.keras.constraints import MaxNorm
+from tensorflow.keras.layers import Conv2D, Bidirectional, LSTM, GRU, Dense
+from tensorflow.keras.layers import Dropout, BatchNormalization, LeakyReLU, PReLU
+from tensorflow.keras.layers import Input, MaxPooling2D, Reshape, TimeDistributed
+from tensorflow.keras.optimizers import RMSprop
 
 
 def bluche(input_size, output_size, learning_rate=4e-4):
@@ -28,26 +28,26 @@ def bluche(input_size, output_size, learning_rate=4e-4):
     input_data = Input(name="input", shape=input_size)
     cnn = Reshape((input_size[0] // 2, input_size[1] // 2, input_size[2] * 4))(input_data)
 
-    cnn = Conv2D(filters=8, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
-    cnn = Activation(activation="tanh")(cnn)
+    cnn = Conv2D(filters=8, kernel_size=(3,3), strides=(1,1), padding="same", activation="tanh")(cnn)
+    cnn = Dropout(rate=0.5)(cnn)
 
-    cnn = Conv2D(filters=16, kernel_size=(2,4), strides=(2,4), padding="same")(cnn)
-    cnn = Activation(activation="tanh")(cnn)
+    cnn = Conv2D(filters=16, kernel_size=(2,4), strides=(2,4), padding="same", activation="tanh")(cnn)
+    cnn = Dropout(rate=0.5)(cnn)
 
     cnn = GatedConv2D(filters=16, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
 
-    cnn = Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
-    cnn = Activation(activation="tanh")(cnn)
+    cnn = Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same", activation="tanh")(cnn)
+    cnn = Dropout(rate=0.5)(cnn)
 
     cnn = GatedConv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
 
-    cnn = Conv2D(filters=64, kernel_size=(2,4), strides=(2,4), padding="same")(cnn)
-    cnn = Activation(activation="tanh")(cnn)
+    cnn = Conv2D(filters=64, kernel_size=(2,4), strides=(2,4), padding="same", activation="tanh")(cnn)
+    cnn = Dropout(rate=0.5)(cnn)
 
     cnn = GatedConv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
 
-    cnn = Conv2D(filters=128, kernel_size=(3,3), strides=(1,1), padding="same")(cnn)
-    cnn = Activation(activation="tanh")(cnn)
+    cnn = Conv2D(filters=128, kernel_size=(3,3), strides=(1,1), padding="same", activation="tanh")(cnn)
+    cnn = Dropout(rate=0.5)(cnn)
 
     cnn = MaxPooling2D(pool_size=(1,4), strides=(1,4), padding="valid")(cnn)
 
@@ -55,13 +55,11 @@ def bluche(input_size, output_size, learning_rate=4e-4):
     blstm = Reshape((shape[1], shape[2] * shape[3]))(cnn)
 
     blstm = Bidirectional(LSTM(units=128, return_sequences=True, dropout=0.5))(blstm)
-    blstm = Dense(units=128)(blstm)
-    blstm = Activation(activation="tanh")(blstm)
+    blstm = Dense(units=128, activation="tanh")(blstm)
 
     blstm = Bidirectional(LSTM(units=128, return_sequences=True, dropout=0.5))(blstm)
-    blstm = Dense(units=output_size)(blstm)
+    output_data = Dense(units=output_size, activation="softmax")(blstm)
 
-    output_data = Activation(activation="softmax")(blstm)
     optimizer = RMSprop(learning_rate=learning_rate)
 
     return (input_data, output_data, optimizer)
@@ -115,9 +113,8 @@ def puigcerver(input_size, output_size, learning_rate=3e-4):
     blstm = Bidirectional(LSTM(units=256, return_sequences=True, dropout=0.5))(blstm)
 
     blstm = Dropout(rate=0.5)(blstm)
-    blstm = Dense(units=output_size)(blstm)
+    output_data = Dense(units=output_size, activation="softmax")(blstm)
 
-    output_data = Activation(activation="softmax")(blstm)
     optimizer = RMSprop(learning_rate=learning_rate)
 
     return (input_data, output_data, optimizer)
@@ -168,15 +165,14 @@ def flor(input_size, output_size, learning_rate=5e-4):
     cnn = MaxPooling2D(pool_size=(1,2), strides=(1,2), padding="valid")(cnn)
 
     shape = cnn.get_shape()
-    blstm = Reshape((shape[1], shape[2] * shape[3]))(cnn)
+    bgru = Reshape((shape[1], shape[2] * shape[3]))(cnn)
 
-    blstm = Bidirectional(LSTM(units=128, return_sequences=True, dropout=0.5))(blstm)
-    blstm = Dense(units=128)(blstm)
+    bgru = Bidirectional(GRU(units=128, return_sequences=True, dropout=0.5))(bgru)
+    bgru = TimeDistributed(Dense(units=128))(bgru)
 
-    blstm = Bidirectional(LSTM(units=128, return_sequences=True, dropout=0.5))(blstm)
-    blstm = Dense(units=output_size)(blstm)
+    bgru = Bidirectional(GRU(units=128, return_sequences=True, dropout=0.5))(bgru)
+    output_data = TimeDistributed(Dense(units=output_size, activation="softmax"))(bgru)
 
-    output_data = Activation(activation="softmax")(blstm)
     optimizer = RMSprop(learning_rate=learning_rate)
 
     return (input_data, output_data, optimizer)
