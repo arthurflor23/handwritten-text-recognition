32052882e189eac633a41d42492b9729d22afab3
Arthur Flôr <arthurflor23@gmail.com>
2020-05-21 02:05:39 -0300

    Update reader

--- src/data/reader.py
+++ src/data/reader.py
@@ -3,12 +3,13 @@
 import os
 import html
 import string
+import multiprocessing
 import xml.etree.ElementTree as ET
 
+from glob import glob
+from tqdm import tqdm
 from data import preproc as pp
 from functools import partial
-from glob import glob
-from multiprocessing import Pool
 
 
 class Dataset():
@@ -23,7 +24,17 @@ class Dataset():
     def read_partitions(self):
         """Read images and sentences from dataset"""
 
-        self.dataset = getattr(self, f"_{self.name}")()
+        dataset = getattr(self, f"_{self.name}")()
+
+        if not self.dataset:
+            self.dataset = dict()
+
+            for y in self.partitions:
+                self.dataset[y] = {'dt': [], 'gt': []}
+
+        for y in self.partitions:
+            self.dataset[y]['dt'] += dataset[y]['dt']
+            self.dataset[y]['gt'] += dataset[y]['gt']
 
     def preprocess_partitions(self, input_size):
         """Preprocess images and sentences from partitions"""
@@ -41,10 +52,16 @@ class Dataset():
 
                 self.dataset[y]['gt'][i] = text.encode()
 
-            pool = Pool()
-            self.dataset[y]['dt'] = pool.map(partial(pp.preprocess, input_size=input_size), self.dataset[y]['dt'])
-            pool.close()
-            pool.join()
+            results = []
+            with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+                print(f"Partition: {y}")
+                for result in tqdm(pool.imap(partial(pp.preprocess, input_size=input_size), self.dataset[y]['dt']),
+                                   total=len(self.dataset[y]['dt'])):
+                    results.append(result)
+                pool.close()
+                pool.join()
+
+            self.dataset[y]['dt'] = results
 
     def _bentham(self):
         """Bentham dataset reader"""
@@ -237,4 +254,4 @@ class Dataset():
 
         punc_percent = (len(strip_punc) - len(no_punc)) / len(strip_punc)
 
-        return len(no_punc) >= 2 and punc_percent <= 0.1
+        return len(no_punc) > 2 and punc_percent <= 0.1
