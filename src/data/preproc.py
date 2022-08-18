ab1606beddcd64fc8f010855a77894221562b99d
Arthur Flôr <arthurflor23@gmail.com>
2022-08-18 12:56:03 -0300

    Add optional preprocessing

--- src/data/preproc.py
+++ src/data/preproc.py
@@ -2,9 +2,9 @@
 Data preproc functions:
     adjust_to_see: adjust image to better visualize (rotate and transpose)
     augmentation: apply variations to a list of images
+    text_standardize: preprocess and standardize sentence
     normalization: apply normalization and variations on images (if required)
     preprocess: main function for preprocess
-    text_standardize: preprocess and standardize sentence
 """
 
 import re
@@ -71,61 +71,6 @@ def augmentation(imgs,
     return imgs
 
 
-def normalization(imgs):
-    """Normalize list of images"""
-
-    imgs = np.asarray(imgs).astype(np.float32)
-    imgs = np.expand_dims(imgs / 255, axis=-1)
-    return imgs
-
-
-"""
-Preprocess metodology based in:
-    H. Scheidl, S. Fiel and R. Sablatnig,
-    Word Beam Search: A Connectionist Temporal Classification Decoding Algorithm, in
-    16th International Conference on Frontiers in Handwriting Recognition, pp. 256-258, 2018.
-"""
-
-
-def preprocess(img, input_size):
-    """Make the process with the `input_size` to the scale resize"""
-
-    def imread(path):
-        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        u, i = np.unique(np.array(img).flatten(), return_inverse=True)
-        background = int(u[np.argmax(np.bincount(i))])
-        return img, background
-
-    if isinstance(img, str):
-        img, bg = imread(img)
-
-    if isinstance(img, tuple):
-        image, boundbox = img
-        img, bg = imread(image)
-
-        for i in range(len(boundbox)):
-            if isinstance(boundbox[i], float):
-                total = len(img) if i < 2 else len(img[0])
-                boundbox[i] = int(total * boundbox[i])
-            else:
-                boundbox[i] = int(boundbox[i])
-
-        img = np.asarray(img[boundbox[0]:boundbox[1], boundbox[2]:boundbox[3]], dtype=np.uint8)
-
-    wt, ht, _ = input_size
-    h, w = np.asarray(img).shape
-    f = max((w / wt), (h / ht))
-
-    new_size = (max(min(wt, int(w / f)), 1), max(min(ht, int(h / f)), 1))
-    img = cv2.resize(img, new_size)
-
-    target = np.ones([ht, wt], dtype=np.uint8) * bg
-    target[0:new_size[1], 0:new_size[0]] = img
-    img = cv2.transpose(target)
-
-    return img
-
-
 """
 DeepSpell based text cleaning process.
     Tal Weiss.
@@ -169,3 +114,69 @@ def text_standardize(text):
     text = NORMALIZE_WHITESPACE_REGEX.sub(" ", text.strip())
 
     return text
+
+
+def normalization(imgs):
+    """Normalize list of images"""
+
+    imgs = np.asarray(imgs).astype(np.float32)
+    _, h, w = imgs.shape
+
+    for i in range(len(imgs)):
+        m, s = cv2.meanStdDev(imgs[i])
+        imgs[i] = imgs[i] - m[0][0]
+        imgs[i] = imgs[i] / s[0][0] if s[0][0] > 0 else imgs[i]
+
+    return np.expand_dims(imgs, axis=-1)
+
+
+"""
+Preprocess metodology based in:
+    H. Scheidl, S. Fiel and R. Sablatnig,
+    Word Beam Search: A Connectionist Temporal Classification Decoding Algorithm, in
+    16th International Conference on Frontiers in Handwriting Recognition, pp. 256-258, 2018.
+"""
+
+
+def preprocess(img, input_size):
+    """Make the process with the `input_size` to the scale resize"""
+
+    def imread(path):
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        u, i = np.unique(np.array(img).flatten(), return_inverse=True)
+        background = int(u[np.argmax(np.bincount(i))])
+        return img, background
+
+    if isinstance(img, str):
+        img, bg = imread(img)
+
+    if isinstance(img, tuple):
+        image, boundbox = img
+        img, bg = imread(image)
+
+        for i in range(len(boundbox)):
+            if isinstance(boundbox[i], float):
+                total = len(img) if i < 2 else len(img[0])
+                boundbox[i] = int(total * boundbox[i])
+            else:
+                boundbox[i] = int(boundbox[i])
+
+        img = np.asarray(img[boundbox[0]:boundbox[1], boundbox[2]:boundbox[3]], dtype=np.uint8)
+
+    wt, ht, _ = input_size
+    h, w = np.asarray(img).shape
+    f = max((w / wt), (h / ht))
+
+    new_size = (max(min(wt, int(w / f)), 1), max(min(ht, int(h / f)), 1))
+    
+    ### Only if you need ###
+    #_, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    img = cv2.resize(img, new_size)
+
+    target = np.ones([ht, wt], dtype=np.uint8) * bg
+    target[0:new_size[1], 0:new_size[0]] = img
+    img = cv2.transpose(target)
+
+    return img
+
