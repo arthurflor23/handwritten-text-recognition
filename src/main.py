27ee3bd78e21e34cd6ad6347771803b745febb2e
Lance Parrish <lanciferp@gmail.com>
2023-01-06 23:19:20 -0500

    added archive support

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ import time
 import os
 import string
 import tarfile
+import fastparquet
+import shutil
+import csv
 
-from zipfile import Zipfile
+from zipfile import ZipFile
 
 from data import preproc as pp
 from data.generator import Tokenizer
@@ -33,15 +36,27 @@ if __name__ == "__main__":
     start_time = time.time()
 
     if args.source:
+        final_predicts = []
 
         if args.archive:
             # Slurm allocates storage space for a job that isn't subject to the same file size and count restrictions
             # as the rest of the storage, so we will unpack the archive onto the tmp folder created.
             archive_path = args.source
             archive_file_type = archive_path.split(".", 1)[1]
-            folder_path = "tmp"
+            folder_path = "temp"
             if archive_file_type == "zip":
-                Zipfile(archive_path).extractall(folder_path)
+                with ZipFile(archive_path) as zip_file:
+                    for member in zip_file.namelist():
+                        filename = os.path.basename(member)
+                        # skip directories
+                        if not filename:
+                            continue
+
+                        # copy file (taken from zipfile's extract)
+                        source = zip_file.open(member)
+                        target = open(os.path.join(folder_path, filename), "wb")
+                        with source, target:
+                            shutil.copyfileobj(source, target)
 
             elif archive_file_type == "tar" or archive_file_type == "tar.gz":
                 tarfile.open(archive_path).extractall(folder_path)
@@ -61,14 +76,21 @@ if __name__ == "__main__":
         model.compile()
         model.load_checkpoint(target=weights_path)
 
-        images = [os.path.join(folder_path, x) for x in os.listdir(folder_path) if x.split(".")[-1] == "jpg" or x.split(".")[-1] == "jp2"]
+        images = [x for x in os.listdir(folder_path) if x.split(".")[-1] == "jpg" or x.split(".")[-1] == "jp2"]
         for image_name in images:
-            img = pp.preprocess(image_name, input_size=input_size)
+            img = pp.preprocess(os.path.join(folder_path, image_name), input_size=input_size)
             x_test = pp.normalization([img])
 
             predicts, probabilities = model.predict(x_test, ctc_decode=True)
             predicts = [[tokenizer.decode(x) for x in y] for y in predicts]
+            final_predicts.append([predicts[0][0], image_name])
 
+        if args.csv:
+            with open('predicts.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(final_predicts)
+        elif args.parquet:
+            fastparquet.write('predicts.parquet', final_predicts)
 
         finish_time = time.time()
         total_time = finish_time - start_time
