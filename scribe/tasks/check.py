5260aa49fee3e9b62341b518d815d26bdaed471d
Arthur Flôr <arthurflor23@gmail.com>
2023-03-28 08:16:48 -0300

    Add check task file

--- scribe/tasks/check.py
+++ scribe/tasks/check.py
@@ -1,9 +1,24 @@
 """Perform validation pipelines on files structure and transformations"""
 
 
+def augmentation(args):
+
+    print("check augmentation", args)
+
+
+def encoding(args):
+
+    print("check encoding", args)
+
+
 def images(args):
 
     print("check images", args)
+
+
+def labels(args):
+
+    print("check labels", args)
 
 
 def source(args):
