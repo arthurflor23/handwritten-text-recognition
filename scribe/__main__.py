b1dda1287025321ab35ec55c0c607a4da696c67e
Arthur Flôr <arthurflor23@gmail.com>
2023-03-28 08:18:14 -0300

    Update main script

--- scribe/__main__.py
+++ scribe/__main__.py
@@ -10,23 +10,24 @@ if __name__ == '__main__':
     # setup parameters
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--source', default=None, help="source data to load (training/testing)")
+    parser.add_argument('--source', default=None, help="define source data for training and testing phases")
+    parser.add_argument('--level', default=None, help="define recognition level (character, word, line, paragraph)")
+    parser.add_argument('--lazy-mode', default=False, action='store_true', help="define data loading on demand")
     parser.add_argument('--source-metadata', default={}, type=json.loads, help="metadata object for source module")
 
-    parser.add_argument('--network', default=None, help="network architecture for recognition")
-    parser.add_argument('--network-metadata', default={}, type=json.loads, help="metadata object for network module")
+    parser.add_argument('--network', default=None, help="define network architecture")
+    # parser.add_argument('--network-metadata', default={}, type=json.loads, help="metadata object for network module")
 
-    parser.add_argument('--train', default=False, action='store_true', help="flag to perform model training")
-    parser.add_argument('--train-metadata', default={}, type=json.loads, help="metadata object for train module")
+    parser.add_argument('--train', default=False, action='store_true', help="perform source training phase")
+    # parser.add_argument('--train-metadata', default={}, type=json.loads, help="metadata object for train module")
 
-    parser.add_argument('--test', default=False, action='store_true', help="flag to perform model test")
-    parser.add_argument('--test-metadata', default={}, type=json.loads, help="metadata object for test module")
+    parser.add_argument('--test', default=False, action='store_true', help="perform source test phase")
+    # parser.add_argument('--test-metadata', default={}, type=json.loads, help="metadata object for test module")
 
-    parser.add_argument('--infer', default=[], nargs='+', help="image path list for recognition")
-    parser.add_argument('--infer-metadata', default={}, type=json.loads, help="metadata object for infer module")
+    parser.add_argument('--infer', default=[], nargs='+', help="recognize handwritten text in list of arbitrary images")
+    # parser.add_argument('--infer-metadata', default={}, type=json.loads, help="metadata object for infer module")
 
-    parser.add_argument('--check-source', default=False, action='store_true', help="validate source data")
-    parser.add_argument('--check-images', default=False, action='store_true', help="visualize images")
+    parser.add_argument('--check', default=None, help="validate data (source, images, labels, augmentation, encoding)")
 
     args = parser.parse_args()
 
@@ -35,18 +36,33 @@ if __name__ == '__main__':
     args.output_path = os.path.join(os.path.dirname(__file__), '..', 'artifacts')
 
     # setup tasks
-    if args.check_source or args.check_augmentation:
-        if args.check_source:
-            tasks.check.source(args)
+    if args.check is not None:
+        assert args.source is not None, "source must be defined"
 
-        if args.check_images:
+        if args.check == 'augmentation':
+            tasks.check.augmentation(args)
+
+        elif args.check == 'encoding':
+            tasks.check.encoding(args)
+
+        elif args.check == 'images':
             tasks.check.images(args)
 
+        elif args.check == 'labels':
+            tasks.check.labels(args)
+
+        elif args.check == 'source':
+            tasks.check.source(args)
+
     else:
+        assert args.network is not None, "network must be defined"
+
         if args.train:
+            assert args.source is not None, "source must be defined"
             tasks.train(args)
 
         if args.test:
+            assert args.source is not None, "source must be defined"
             tasks.test(args)
 
         if args.infer and len(args.infer):
