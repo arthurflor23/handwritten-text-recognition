27cfd2a8a86c96a6fbde92923fe76028cf9f8595
Arthur Flôr <arthurflor23@gmail.com>
2023-05-23 23:07:52 -0300

    Improve code

--- graphite/dataset/dataset.py
+++ graphite/dataset/dataset.py
@@ -25,91 +25,98 @@ class Dataset():
         self.input_path = input_path
         self.seed = seed
 
+        data = self._fetch_data()
+
         self.training = {
             'index': 0,
-            'labels': [],
-            'images': [],
-            'cropping': [],
+            'labels': data[0][0],
+            'images': data[0][1],
+            'cropping': data[0][2],
         }
 
         self.validation = {
             'index': 0,
-            'labels': [],
-            'images': [],
-            'cropping': [],
+            'labels': data[1][0],
+            'images': data[1][1],
+            'cropping': data[1][2],
         }
 
         self.test = {
             'index': 0,
-            'labels': [],
-            'images': [],
-            'cropping': [],
+            'labels': data[2][0],
+            'images': data[2][1],
+            'cropping': data[2][2],
         }
 
-        data = self._fetch_data()
-
-        for x in data:
-            print(x)
+        print(self.training)
+        print(self.validation)
+        print(self.test)
 
     def __repr__(self):
         return "temp 1"
 
     def _fetch_data(self):
+        # Get the module based on the source
         module_name = f"dataset.source.{self.source}"
+        module_spec = importlib.util.find_spec(module_name)
+        assert module_spec is not None, "source must be created"
 
-        assert importlib.util.find_spec(module_name) is not None, "source must be created"
         module = importlib.import_module(module_name)
 
+        # Get the method based on the level
         method_name = f"get_{self.level}_data"
+        assert hasattr(module, method_name), f"`{method_name}` method must be created"
 
-        assert hasattr(module, method_name), "`get_<level>_data` method must be created"
         method = getattr(module, method_name)
 
+        # Call the method to get the data
         data = method(self.input_path)
-        data = self._validate_data(data)
 
-        return data
+        # Perform data validation checks
+        assert data is not None and 1 <= len(data) <= 3, "data must have 3 dims (training, validation, test)"
 
-    def _validate_data(self, data):
-        assert data is not None, "no data"
-        assert len(data) >= 1 and len(data) <= 3, "data in wrong format"
-
-        if type(data) is tuple:
+        if isinstance(data, tuple):
             data = list(data)
 
         data.extend([[[], [], []]] * (3 - len(data)))
 
         for i in range(len(data)):
-            if data[i] is None:
-                data[i] = [[], [], []]
-
+            data[i] = data[i] or [[], [], []]
             data[i].extend([[[], [], []]] * (3 - len(data[i])))
-            assert len(data[i]) >= 1 and len(data[i]) <= 3, "data in wrong format"
+            assert 1 <= len(data[i]) <= 3, "partitions must have 3 dims (labels, images, cropping)"
 
-            if data[i][2] is None:
-                data[i][2] = []
+            data[i][2] = data[i][2] or []
 
-            elif len(data[i][2]) == 0:
+            if len(data[i][2]) == 0:
                 data[i][2] = [[]] * len(data[i][1])
 
             elif len(data[i][2]) == 1:
                 data[i][2] = data[i][2] * len(data[i][1])
 
-            assert len(data[i][0]) == len(data[i][1])\
-                and len(data[i][2]) == len(data[i][1]), "data lengths do not match"
+            assert len(data[i][0]) == len(data[i][1]) == len(data[i][2]), "dims must have the same length"
 
+            sum_crop = len([x for x in data[i][2] if len(x) > 0])
+            sum_crop_dims = sum(len(x) for x in data[i][2])
+
+            assert sum_crop == 0 or sum_crop_dims == (sum_crop * 4), "cropping must have 4 dimensions"
+
+        # Get the training, validation, and test ratios
         ratios = [self.training_ratio, self.validation_ratio, self.test_ratio]
 
         for i in range(len(ratios)):
             if ratios[i] is not None:
                 ratios[i] = float(ratios[i]) if '.' in ratios[i] else int(ratios[i])
 
+        # Calculate the total ratio
         ratio = sum(x for x in ratios if x is not None)
 
+        # Set the random seed
         random.seed(self.seed)
+
+        # Convert data to a list of tuples
         data = [list(zip(x[0], x[1], x[2])) for x in data]
 
-        if type(ratio) is float and ratio == 1.0:
+        if isinstance(ratio, float) and ratio == 1.0:
             merged = []
 
             for i, ratio in enumerate(ratios):
@@ -124,7 +131,7 @@ class Dataset():
                 random.shuffle(data[i])
 
                 if ratio is not None:
-                    index = int(ratio * total_merged)
+                    index = round(ratio * total_merged)
                     data[i] = merged[:index]
                     merged[:index] = []
 
@@ -133,11 +140,10 @@ class Dataset():
                 random.shuffle(data[i])
 
                 if ratio is not None:
-                    index = int(ratio * len(data[i])) if type(ratio) is float else ratio
+                    index = round(ratio * len(data[i])) if isinstance(ratio, float) else ratio
                     data[i] = data[i][:index]
 
-        for i in range(len(data)):
-            unzipped = list(zip(*data[i]))
-            data[i] = [list(x) for x in unzipped] if len(unzipped) else [[], [], []]
+        # Unzip the data and convert to lists
+        data = [[list(x) for x in zip(*d)] if d else [[], [], []] for d in data]
 
         return data
