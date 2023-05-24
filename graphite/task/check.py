1716734aadddb5043d70688a953561fbe7f63824
Arthur Flôr <arthurflor23@gmail.com>
2023-05-24 00:04:45 -0300

    Add initial code

--- graphite/task/check.py
+++ graphite/task/check.py
@@ -1,4 +1,17 @@
+from dataset import Dataset
+
 
 def check(args):
 
-    print("check data", args)
+    # print("check data", args)
+
+    dataset = Dataset(source=args.source,
+                      level=args.level,
+                      training_ratio=args.training_ratio,
+                      validation_ratio=args.validation_ratio,
+                      test_ratio=args.test_ratio,
+                      lazy_mode=args.lazy_mode,
+                      data_path=args.data_path,
+                      seed=42)
+
+    # print(dataset)
