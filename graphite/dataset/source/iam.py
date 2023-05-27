f30887e153fc18277ac944c528becf27fc2595ff
Arthur Flôr <arthurflor23@gmail.com>
2023-05-27 13:26:24 -0300

    Add IAM source line method

--- graphite/dataset/source/iam.py
+++ graphite/dataset/source/iam.py
@@ -34,23 +34,109 @@ class Source():
         self.lines_file_path = os.path.join(self.transcription_path, 'lines.txt')
 
     def get_line_data(self):
-        factor = 1
+        """
+        Retrieves the line data for training, validation, and testing.
 
-        images = [os.path.join(self.data_path, 'sample.png'), os.path.join(self.data_path, 'sample.png')] * factor
-        # labels = [". . . this is #80 calling . . . '", ". . . this is #80 calling . . . '"] * factor
-        # labels = [[". . . this is #80 calling . . . '"], [". . . this is #80 calling . . . '"]] * factor
-        labels = ["Mon habitation étant assurée par votre\nsociété,\tj&apos;aimerais recevoir une",
-                  ". . . this is #80 calling . . . '"] * factor
-        # bbox = [[0, 0, 0, 0]]
-        bbox = []
+        Returns
+        -------
+        tuple
+            A tuple containing lists of training, validation, and test lines data.
+        """
 
-        training = [images, None, labels]
+        training_partition_data = self._load_partition_data(self.training_file_path)
+        validation_partition_data = self._load_partition_data(self.validation_file_path)
+        test_partition_data = self._load_partition_data(self.test_file_path)
 
-        validation = []
-        test = []
+        lines_data = self._load_lines_data(self.lines_file_path)
 
-        # return images
-        # return [images, labels]
-        # return [images, bbox, labels]
-        # return training
-        return training, validation
+        training_lines = self._filter_lines_data(lines_data, training_partition_data)
+        validation_lines = self._filter_lines_data(lines_data, validation_partition_data)
+        test_lines = self._filter_lines_data(lines_data, test_partition_data)
+
+        return training_lines, validation_lines, test_lines
+
+    def _load_partition_data(self, file_path):
+        """
+        Loads the partition data.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the file containing the partition data.
+
+        Returns
+        -------
+        list
+            A list of partition data.
+        """
+
+        with open(file_path, 'r') as file:
+            data = [line.strip() for line in file.readlines()]
+
+        return data
+
+    def _load_lines_data(self, file_path):
+        """
+        Loads the lines data from a file.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the file containing the lines data.
+
+        Returns
+        -------
+        list
+            A list of lines data.
+        """
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+        lines_data = []
+
+        for line in lines:
+            if line.startswith('#'):
+                continue
+
+            parts = line.split()
+
+            line_ids = parts[0].split('-')
+            line_path = os.path.join('lines', line_ids[0], '-'.join(line_ids[:2]))
+            line_file_name = f"{parts[0]}.png"
+
+            image_path = os.path.join(self.base_path, line_path, line_file_name)
+            bbox = []  # [int(x) for x in parts[6:8] + parts[4:6]]
+            label = ' '.join(parts[8:]).replace('|', ' ')
+
+            lines_data.append([image_path, bbox, label])
+
+        return lines_data
+
+    def _filter_lines_data(self, lines_data, partition_data):
+        """
+        Filters the lines data based on the partition data.
+
+        Parameters
+        ----------
+        lines_data : list
+            The lines data to filter.
+
+        partition_data : list
+            The partition data to use for filtering.
+
+        Returns
+        -------
+        list
+            The filtered lines data.
+        """
+
+        filtered_data = []
+
+        for line_id in partition_data:
+            for line_data in lines_data:
+                if line_id in line_data[0]:
+                    filtered_data.append(line_data)
+                    break
+
+        return filtered_data
