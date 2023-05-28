3e6cfd5ec58fad74d5ce6634a6b679abd1e5bec4
Arthur Flôr <arthurflor23@gmail.com>
2023-05-27 23:19:23 -0300

    Add Bentham source class

---
+++ graphite/dataset/source/bentham.py
@@ -0,0 +1,248 @@
+import os
+import glob
+import multiprocessing
+import xml.etree.ElementTree as ET
+
+
+class Source():
+    """
+    Represents the Bentham database source.
+    """
+
+    def __init__(self, data_path):
+        """
+        Initializes a new instance of the Source class.
+
+        Parameters
+        ----------
+        data_path : str
+            The path to the data.
+
+        Returns
+        -------
+        None
+        """
+
+        self.data_path = data_path
+        self.base_path = os.path.join(self.data_path, 'bentham')
+
+        self.partition_path = os.path.join(self.base_path, 'Partitions')
+        self.training_file_path = os.path.join(self.partition_path, 'Train.lst')
+        self.validation_file_path = os.path.join(self.partition_path, 'Validation.lst')
+        self.test_file_path = os.path.join(self.partition_path, 'Test.lst')
+
+        self.lines_file_path = os.path.join(self.base_path, 'Transcriptions', '**.txt')
+        self.paragraphs_file_path = os.path.join(self.base_path, 'PAGE', '**.xml')
+
+    def get_line_data(self):
+        """
+        Retrieves the line data for training, validation, and testing.
+
+        Returns
+        -------
+        tuple
+            A tuple containing lists of training, validation, and test lines data.
+        """
+
+        # Load the partition data for training, validation, and testing
+        training_partition_data = self._load_partition_data(self.training_file_path)
+        validation_partition_data = self._load_partition_data(self.validation_file_path)
+        test_partition_data = self._load_partition_data(self.test_file_path)
+
+        # Load the lines data from the files
+        lines_data = self._load_lines_data(self.lines_file_path)
+
+        # Filter the lines data based on the partition data
+        training_lines = self._filter_data(lines_data, training_partition_data)
+        validation_lines = self._filter_data(lines_data, validation_partition_data)
+        test_lines = self._filter_data(lines_data, test_partition_data)
+
+        return training_lines, validation_lines, test_lines
+
+    def get_paragraph_data(self):
+        """
+        Retrieves the paragraph data for training, validation, and testing.
+
+        Returns
+        -------
+        tuple
+            A tuple containing lists of training, validation, and test paragraphs data.
+        """
+
+        # Load the partition data for training, validation, and testing
+        training_partition_data = self._load_partition_data(self.training_file_path)
+        validation_partition_data = self._load_partition_data(self.validation_file_path)
+        test_partition_data = self._load_partition_data(self.test_file_path)
+
+        # Load the paragraphs data from the files
+        paragraphs_data = self._load_paragraphs_data(self.paragraphs_file_path)
+
+        # Filter the paragraphs data based on the partition data
+        training_paragraphs = self._filter_data(paragraphs_data, training_partition_data)
+        validation_paragraphs = self._filter_data(paragraphs_data, validation_partition_data)
+        test_paragraphs = self._filter_data(paragraphs_data, test_partition_data)
+
+        return training_paragraphs, validation_paragraphs, test_paragraphs
+
+    def _load_partition_data(self, file_path):
+        """
+        Loads the partition data.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the file containing the partition data.
+
+        Returns
+        -------
+        list
+            A list of partition data.
+        """
+
+        with open(file_path, 'r') as file:
+            data = [row.strip() for row in file.readlines()]
+
+        return data
+
+    def _filter_data(self, data, partition_data):
+        """
+        Filter the given data based on the partition data using multiprocessing.
+
+        Parameters
+        ----------
+        data : list
+            The data to filter.
+        partition_data : list
+            The partition data to match against.
+
+        Returns
+        -------
+        list
+            The filtered data that matches the partition data.
+        """
+
+        with multiprocessing.get_context('fork').Pool() as pool:
+            arguments = [(x, partition_data) for x in data]
+            data = [x for x in pool.starmap(self._validate_filtered_data, arguments) if x]
+
+        return data
+
+    def _validate_filtered_data(self, item, partition_data):
+        """
+        Validate if the given item matches any partition data.
+
+        Parameters
+        ----------
+        item : any
+            The item to validate.
+        partition_data : list
+            The partition data to match against.
+
+        Returns
+        -------
+        any or None
+            The item if it matches any partition data, or None if there is no match.
+        """
+
+        filtered_data = None
+
+        for image_id in partition_data:
+            if image_id in item[0]:
+                filtered_data = item
+
+        return filtered_data
+
+    def _load_lines_data(self, file_path):
+        """
+        Loads the lines data from a file.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the file containing the lines data.
+
+        Returns
+        -------
+        list
+            A list of lines data.
+        """
+
+        txt_files = glob.glob(file_path, recursive=True)
+        line_data = []
+
+        for txt_path in txt_files:
+            image_path = os.path.basename(txt_path).replace('.txt', '')
+            image_path = os.path.join(self.base_path, 'Images', 'Lines', f"{image_path}.png")
+
+            with open(txt_path, 'r') as file:
+                label = file.readline().replace('\n', '').strip()
+
+            line_data.append([image_path, [], label])
+
+        return line_data
+
+    def _load_paragraphs_data(self, file_path):
+        """
+        Loads the paragraphs data from a file.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the file containing the lines data.
+
+        Returns
+        -------
+        list
+            A list of paragraphs data.
+        """
+
+        xml_files = glob.glob(file_path, recursive=True)
+        paragraph_data = []
+
+        for xml_path in xml_files:
+            image_path = os.path.basename(xml_path).replace('.xml', '')
+            image_path = os.path.join(self.base_path, 'Images', 'Pages', f"{image_path}.jpg")
+
+            # Parse the XML file
+            root = ET.parse(xml_path).getroot()
+
+            # Define the namespace used in the XML
+            namespace = {'ns': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-03-19'}
+
+            label = []
+            min_x, max_x = float('inf'), float('-inf')
+            min_y, max_y = float('inf'), float('-inf')
+
+            # Find all TextLine elements and iterate over them
+            for text_line in root.findall('.//ns:TextLine', namespace):
+                coords = text_line.find('ns:Coords', namespace)
+                points = coords.get('points')
+                x_values, y_values = [], []
+
+                if points:
+                    # Extract x and y values from the points
+                    for point in points.split(' '):
+                        x, y = point.split(',')
+                        x_values.append(int(x))
+                        y_values.append(int(y))
+
+                else:
+                    # Extract x and y values from the individual points
+                    x_values = [int(point.attrib['x']) for point in coords]
+                    y_values = [int(point.attrib['y']) for point in coords]
+
+                # Get the minimum and maximum coords x and y
+                min_x = min(min_x, min(x_values))
+                max_x = max(max_x, max(x_values))
+                min_y = min(min_y, min(y_values))
+                max_y = max(max_y, max(y_values))
+
+                # Extract the label from the TextEquiv element
+                text_equiv = text_line.find('ns:TextEquiv', namespace)
+                label.append(text_equiv.find('ns:Unicode', namespace).text.strip())
+
+            # Create a paragraph entry with image path, bounding box, and label
+            bbox = [min_x, min_y, max_x - min_x, max_y - min_y]
+            paragraph_data.append([image_path, bbox, label])
+
+        return paragraph_data
