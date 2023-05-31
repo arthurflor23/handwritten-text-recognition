7ee21e3162217acef7cd121b6bedc1afa9f1c77f
Arthur Flôr <arthurflor23@gmail.com>
2023-05-30 21:10:39 -0300

    Update augmentor class

--- graphite/dataset/augmentor.py
+++ graphite/dataset/augmentor.py
@@ -3,11 +3,14 @@ import json
 import numpy as np
 import concurrent.futures
 
+from PIL import Image
+
 
 class Augmentor():
 
     def __init__(self,
                  mixup=None,
+                 erosion=None,
                  dilation=None,
                  elastic_distortion=None,
                  perspective_transform=None,
@@ -20,6 +23,7 @@ class Augmentor():
                  translate_y=None):
 
         self.mixup_params = mixup
+        self.erosion_params = erosion
         self.dilation_params = dilation
         self.elastic_distortion_params = elastic_distortion
         self.perspective_transform_params = perspective_transform
@@ -43,6 +47,7 @@ class Augmentor():
 
         return json.dumps({
             'mixup': self.mixup_params,
+            'erosion': self.erosion_params,
             'dilation': self.dilation_params,
             'elastic_distortion': self.elastic_distortion_params,
             'perspective_transform': self.perspective_transform_params,
@@ -68,6 +73,7 @@ class Augmentor():
         info = f"""
             Augmentor Configuration\n
             Mixup                   {self.mixup_params}
+            Erosion                 {self.erosion_params}
             Dilation                {self.dilation_params}
             Elastic Distortion      {self.elastic_distortion_params}
             Perspective Transform   {self.perspective_transform_params}
@@ -134,51 +140,55 @@ class Augmentor():
 
         """
 
-        # Mixup
-        if batch_images and self.mixup_params and np.random.random() < self.mixup_params[0]:
-            image = self.mixup(image, batch_images, *self.mixup_params[1:])
+        # # Mixup
+        # if batch_images and self.mixup_params and np.random.random() < self.mixup_params[0]:
+        #     image = self.mixup(image, batch_images, *self.mixup_params[1:])
 
-        # Dilation
-        if self.dilation_params and np.random.random() < self.dilation_params[0]:
-            image = self.dilation(image, *self.dilation_params[1:])
+        # # Erosion
+        # if self.erosion_params and np.random.random() < self.erosion_params[0]:
+        #     image = self.erosion(image, *self.erosion_params[1:])
+
+        # # Dilation
+        # if self.dilation_params and np.random.random() < self.dilation_params[0]:
+        #     image = self.dilation(image, *self.dilation_params[1:])
 
         # Elastic Distortions
         if self.elastic_distortion_params and np.random.random() < self.elastic_distortion_params[0]:
             image = self.elastic_distortion(image, *self.elastic_distortion_params[1:])
 
-        # Perspective Transforms
-        if self.perspective_transform_params and np.random.random() < self.perspective_transform_params[0]:
-            image = self.perspective_transform(image, *self.perspective_transform_params[1:])
+        # # Perspective Transforms
+        # if self.perspective_transform_params and np.random.random() < self.perspective_transform_params[0]:
+        #     image = self.perspective_transform(image, *self.perspective_transform_params[1:])
 
-        # Gaussian Noise
-        if self.gaussian_noise_params and np.random.random() < self.gaussian_noise_params[0]:
-            image = self.gaussian_noise(image, *self.gaussian_noise_params[1:])
+        # # Gaussian Noise
+        # if self.gaussian_noise_params and np.random.random() < self.gaussian_noise_params[0]:
+        #     image = self.gaussian_noise(image, *self.gaussian_noise_params[1:])
 
-        # Gaussian Blur
-        if self.gaussian_blur_params and np.random.random() < self.gaussian_blur_params[0]:
-            image = self.gaussian_blur(image, *self.gaussian_blur_params[1:])
+        # # Gaussian Blur
+        # if self.gaussian_blur_params and np.random.random() < self.gaussian_blur_params[0]:
+        #     image = self.gaussian_blur(image, *self.gaussian_blur_params[1:])
 
-        # Shearing
-        if self.shearing_params and np.random.random() < self.shearing_params[0]:
-            image = self.shearing(image, *self.shearing_params[1:])
+        # # Shearing
+        # if self.shearing_params and np.random.random() < self.shearing_params[0]:
+        #     image = self.shearing(image, *self.shearing_params[1:])
 
-        # Scaling
-        if self.scaling_params and np.random.random() < self.scaling_params[0]:
-            image = self.scaling(image, *self.scaling_params[1:])
+        # # Scaling
+        # if self.scaling_params and np.random.random() < self.scaling_params[0]:
+        #     image = self.scaling(image, *self.scaling_params[1:])
 
-        # Rotation
-        if self.rotation_params and np.random.random() < self.rotation_params[0]:
-            image = self.rotation(image, *self.rotation_params[1:])
+        # # Rotation
+        # if self.rotation_params and np.random.random() < self.rotation_params[0]:
+        #     image = self.rotation(image, *self.rotation_params[1:])
 
-        # TranslateX
-        if self.translate_x_params and np.random.random() < self.translate_x_params[0]:
-            image = self.translate_x(image, *self.translate_x_params[1:])
+        # # TranslateX
+        # if self.translate_x_params and np.random.random() < self.translate_x_params[0]:
+        #     image = self.translate_x(image, *self.translate_x_params[1:])
 
-        # TranslateY
-        if self.translate_y_params and np.random.random() < self.translate_y_params[0]:
-            image = self.translate_y(image, *self.translate_y_params[1:])
+        # # TranslateY
+        # if self.translate_y_params and np.random.random() < self.translate_y_params[0]:
+        #     image = self.translate_y(image, *self.translate_y_params[1:])
 
-        return image
+        return np.array(image)
 
     def mixup(self, image, batch_images, opacity, pickups, radius=True):
         """
@@ -220,7 +230,37 @@ class Augmentor():
 
             image = cv2.addWeighted(image, 1 - pickup_opac, pickup_img, pickup_opac, 0)
 
-        return image
+        return np.array(image)
+
+    def erosion(self, image, kernel_size, iterations, radius=True):
+        """
+        Apply Erosion to the image.
+
+        Parameters
+        ----------
+        image : ndarray
+            Input image to be eroded.
+        kernel_size : int
+            Kernel size for erosion.
+        iterations : int
+            Number of iterations for erosion.
+        radius : bool, optional
+            Whether to use range radius for kernel size and iterations, by default True.
+
+        Returns
+        -------
+        ndarray
+            Eroded image.
+        """
+
+        if radius:
+            kernel_size = np.random.randint(1, kernel_size + 1)
+            iterations = np.random.randint(0, iterations + 1)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
+        image = cv2.erode(image, kernel, iterations=iterations)
+
+        return np.array(image)
 
     def dilation(self, image, kernel_size, iterations, radius=True):
         """
@@ -243,10 +283,14 @@ class Augmentor():
             Dilated image.
         """
 
-        # do something
-        # ...
+        if radius:
+            kernel_size = np.random.randint(1, kernel_size + 1)
+            iterations = np.random.randint(0, iterations + 1)
 
-        return image
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
+        image = cv2.dilate(image, kernel, iterations=iterations)
+
+        return np.array(image)
 
     def elastic_distortion(self, image, grid_size, magnitude, radius=True):
         """
@@ -269,10 +313,70 @@ class Augmentor():
             Distorted image.
         """
 
-        # do something
-        # ...
+        if radius:
+            grid_size = np.random.randint(1, grid_size + 1)
+            magnitude = np.random.randint(1, magnitude + 1)
 
-        return image
+        height, width = image.shape[:2]
+
+        horizontal_tiles = grid_size
+        vertical_tiles = grid_size
+
+        width_of_square = width // horizontal_tiles
+        height_of_square = height // vertical_tiles
+
+        width_of_last_square = width - width_of_square * (horizontal_tiles - 1)
+        height_of_last_square = height - height_of_square * (vertical_tiles - 1)
+
+        dimensions = []
+        for vertical_tile in range(vertical_tiles):
+            for horizontal_tile in range(horizontal_tiles):
+                x1 = horizontal_tile * width_of_square
+                y1 = vertical_tile * height_of_square
+
+                if vertical_tile == (vertical_tiles - 1) and horizontal_tile == (horizontal_tiles - 1):
+                    dimensions.append([x1, y1, x1 + width_of_last_square, y1 + height_of_last_square])
+
+                elif vertical_tile == (vertical_tiles - 1):
+                    dimensions.append([x1, y1, x1 + width_of_square, y1 + height_of_last_square])
+
+                elif horizontal_tile == (horizontal_tiles - 1):
+                    dimensions.append([x1, y1, x1 + width_of_last_square, y1 + height_of_square])
+
+                else:
+                    dimensions.append([x1, y1, x1 + width_of_square, y1 + height_of_square])
+
+        last_column = [(horizontal_tiles - 1) + horizontal_tiles * i for i in range(vertical_tiles)]
+        last_row = range((horizontal_tiles * vertical_tiles) - horizontal_tiles, horizontal_tiles * vertical_tiles)
+
+        polygons = [[x1, y1, x1, y2, x2, y2, x2, y1] for x1, y1, x2, y2 in dimensions]
+
+        polygon_indices = [[i, i + 1, i + horizontal_tiles, i + 1 + horizontal_tiles]
+                           for i in range((vertical_tiles * horizontal_tiles) - 1)
+                           if i not in last_row and i not in last_column]
+
+        for a, b, c, d in polygon_indices:
+            dx = np.random.randint(-magnitude, magnitude + 1)
+            dy = np.random.randint(-magnitude, magnitude + 1)
+
+            x1, y1, x2, y2, x3, y3, x4, y4 = polygons[a]
+            polygons[a] = [x1, y1, x2, y2, x3 + dx, y3 + dy, x4, y4]
+
+            x1, y1, x2, y2, x3, y3, x4, y4 = polygons[b]
+            polygons[b] = [x1, y1, x2 + dx, y2 + dy, x3, y3, x4, y4]
+
+            x1, y1, x2, y2, x3, y3, x4, y4 = polygons[c]
+            polygons[c] = [x1, y1, x2, y2, x3, y3, x4 + dx, y4 + dy]
+
+            x1, y1, x2, y2, x3, y3, x4, y4 = polygons[d]
+            polygons[d] = [x1 + dx, y1 + dy, x2, y2, x3, y3, x4, y4]
+
+        mesh = [[dimensions[i], polygons[i]] for i in range(len(dimensions))]
+
+        image = Image.fromarray(image)
+        image = image.transform(image.size, Image.MESH, mesh, resample=Image.BICUBIC, fillcolor=255)
+
+        return np.array(image)
 
     def perspective_transform(self, image, type, magnitude, radius=True):
         """
@@ -298,7 +402,7 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
 
     def gaussian_noise(self, image, kernel_size, iterations, radius=True):
         """
@@ -324,7 +428,7 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
 
     def gaussian_blur(self, image, kernel_size, iterations, radius=True):
         """
@@ -350,7 +454,7 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
 
     def shearing(self, image, value, radius=True):
         """
@@ -374,7 +478,7 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
 
     def scaling(self, image, value, radius=True):
         """
@@ -398,7 +502,7 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
 
     def rotation(self, image, value, radius=True):
         """
@@ -422,7 +526,7 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
 
     def translate_x(self, image, value, radius=True):
         """
@@ -446,7 +550,7 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
 
     def translate_y(self, image, value, radius=True):
         """
@@ -470,4 +574,4 @@ class Augmentor():
         # do something
         # ...
 
-        return image
+        return np.array(image)
