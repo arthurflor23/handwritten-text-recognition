2e9e8ce34dc2d0a3492482e0cba7ed5bed9f1684
Grant Lewis <61807213+grantslewis@users.noreply.github.com>
2023-05-31 16:36:38 -0600

    updated image list and emptying list

--- src/main.py
+++ src/main.py
@@ -137,6 +137,8 @@ if __name__ == "__main__":
                 
         final_predicts = []
 
+        images = []
+        
         if args.archive:
             # Slurm allocates storage space for a job that isn't subject to the same file size and count restrictions
             # as the rest of the storage, so we will unpack the archive onto the tmp folder created.
@@ -156,6 +158,7 @@ if __name__ == "__main__":
                         target = open(os.path.join(folder_path, filename), "wb")
                         with source, target:
                             shutil.copyfileobj(source, target)
+                        images.append(filename)
 
             elif archive_file_type == "tar" or archive_file_type == "tar.gz":
                 tar = tarfile.open(archive_path)
@@ -163,12 +166,14 @@ if __name__ == "__main__":
                     if member.isreg():  # skip if the TarInfo is not files
                         member.name = os.path.basename(member.name)  # remove the path by reset it
                         tar.extract(member, folder_path)  # extract
+                        images.append(member.name)
 
             else:
                 print("Invalid File type, accepted file types are zip, tar, and tar.gz")
 
         else:
             folder_path = args.source
+            images = os.listdir(folder_path)
 
         tokenizer = Tokenizer(chars=charset_base, max_text_length=max_text_length)
         model = HTRModel(architecture=args.arch,
@@ -183,8 +188,10 @@ if __name__ == "__main__":
 
         blank_model = xgb.XGBClassifier()
         blank_model.load_model("./blank_detector.json")
-
-        images = [x for x in os.listdir(folder_path) if x.split(".")[-1] == "jpg" or x.split(".")[-1] == "jp2"]
+        
+        
+        images = [x for x in images if x.split(".")[-1] == "jpg" or x.split(".")[-1] == "jp2"]
+        # images = [x for x in os.listdir(folder_path) if x.split(".")[-1] == "jpg" or x.split(".")[-1] == "jp2"]
         if args.test:
             images = images[:args.test]
 
@@ -195,8 +202,8 @@ if __name__ == "__main__":
         pbar = tqdm(images) # tqdm(total=total)
 
         # for image_name in images:
-        # for i, image_name in enumerate(pbar):
-        for image_name in pbar: 
+        for i, image_name in enumerate(pbar):
+        # for image_name in pbar: 
             pbar.set_description(f'{image_name}') # f'Image: {image_name} ({i})'
             # print(f'{image_name} ({i} / {total})')
             
@@ -211,7 +218,7 @@ if __name__ == "__main__":
                 continue
 
             if img is None:
-                    continue
+                continue
 
             # first check if image is a blank snippet
             vertical_crop = int(img.shape[0] * 0.1375)
@@ -261,6 +268,19 @@ if __name__ == "__main__":
 
             final_predicts.append([image_name, predicts[0][0], probabilities[0][0], predicted_blank])
             # pbar.update(1)
+            if i != 0 and i % BATCH_SIZE == 0:
+                if args.csv:
+                    if args.csv.split(".")[-1] != "csv":
+                        csv_path = os.path.join(args.csv, "predicts.csv")
+                    else:
+                        csv_path = args.csv
+                    with open(csv_path, 'a', newline='') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerows(final_predicts)
+                elif args.parquet:
+                    parquet_path = os.path.join(args.csv, 'predicts.parquet')
+                    fastparquet.write(parquet_path, final_predicts)
+                final_predicts = []
 
         if args.csv:
             if args.csv.split(".")[-1] != "csv":
