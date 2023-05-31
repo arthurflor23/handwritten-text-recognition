261bd8e6f2f519e031c4a4ee63d6699a3f4e86d4
Arthur Flôr <arthurflor23@gmail.com>
2023-05-31 09:44:58 -0300

    Update augmentor class

--- graphite/dataset/augmentor.py
+++ graphite/dataset/augmentor.py
@@ -7,6 +7,9 @@ from PIL import Image
 
 
 class Augmentor():
+    """
+    Image augmentation class for applying various transformations to images.
+    """
 
     def __init__(self,
                  mixup=None,
@@ -14,26 +17,66 @@ class Augmentor():
                  dilation=None,
                  elastic_distortion=None,
                  perspective_transform=None,
-                 gaussian_noise=None,
+                 salt_and_pepper=None,
                  gaussian_blur=None,
                  shearing=None,
                  scaling=None,
                  rotation=None,
                  translate_x=None,
-                 translate_y=None):
+                 translate_y=None,
+                 seed=None):
+        """
+        Initializes a new instance of the Augmentor class.
+
+        Parameters
+        ----------
+        mixup : dict or None, optional
+            Parameters for mixup transformation, by default None.
+        erosion : dict or None, optional
+            Parameters for erosion transformation, by default None.
+        dilation : dict or None, optional
+            Parameters for dilation transformation, by default None.
+        elastic_distortion : dict or None, optional
+            Parameters for elastic distortion transformation, by default None.
+        perspective_transform : dict or None, optional
+            Parameters for perspective transform transformation, by default None.
+        salt_and_pepper : dict or None, optional
+            Parameters for salt and pepper noise, by default None.
+        gaussian_blur : dict or None, optional
+            Parameters for Gaussian blur transformation, by default None.
+        shearing : dict or None, optional
+            Parameters for shearing transformation, by default None.
+        scaling : dict or None, optional
+            Parameters for scaling transformation, by default None.
+        rotation : dict or None, optional
+            Parameters for rotation transformation, by default None.
+        translate_x : dict or None, optional
+            Parameters for horizontal translation transformation, by default None.
+        translate_y : dict or None, optional
+            Parameters for vertical translation transformation, by default None.
+        seed : int or None, optional
+            Seed for random number generation, by default None.
+
+        Returns
+        -------
+        None
+        """
+
+        np.random.seed(seed)
 
         self.mixup_params = mixup
         self.erosion_params = erosion
         self.dilation_params = dilation
         self.elastic_distortion_params = elastic_distortion
         self.perspective_transform_params = perspective_transform
-        self.gaussian_noise_params = gaussian_noise
+        self.salt_and_pepper_params = salt_and_pepper
         self.gaussian_blur_params = gaussian_blur
         self.shearing_params = shearing
         self.scaling_params = scaling
         self.rotation_params = rotation
         self.translate_x_params = translate_x
         self.translate_y_params = translate_y
+        self.seed = seed
 
     def __repr__(self):
         """
@@ -51,13 +94,14 @@ class Augmentor():
             'dilation': self.dilation_params,
             'elastic_distortion': self.elastic_distortion_params,
             'perspective_transform': self.perspective_transform_params,
-            'gaussian_noise': self.gaussian_noise_params,
+            'salt_and_pepper': self.salt_and_pepper_params,
             'gaussian_blur': self.gaussian_blur_params,
             'shearing': self.shearing_params,
             'scaling': self.scaling_params,
             'rotation': self.rotation_params,
             'translate_x': self.translate_x_params,
             'translate_y': self.translate_y_params,
+            'seed': self.seed,
         })
 
     def __str__(self):
@@ -78,7 +122,7 @@ class Augmentor():
             Elastic Distortion      {self.elastic_distortion_params}
             Perspective Transform   {self.perspective_transform_params}
 
-            Gaussian Noise          {self.gaussian_noise_params}
+            Salt and Pepper Noise   {self.salt_and_pepper_params}
             Gaussian Blur           {self.gaussian_blur_params}
 
             Shearing                {self.shearing_params}
@@ -87,6 +131,8 @@ class Augmentor():
             Rotation                {self.rotation_params}
             Translation X           {self.translate_x_params}
             Translation Y           {self.translate_y_params}
+
+            Seed                    {self.seed}
         """
 
         info = '\n'.join([x.strip() for x in info.splitlines()])
@@ -152,21 +198,21 @@ class Augmentor():
         # if self.dilation_params and np.random.random() < self.dilation_params[0]:
         #     image = self.dilation(image, *self.dilation_params[1:])
 
-        # Elastic Distortions
-        if self.elastic_distortion_params and np.random.random() < self.elastic_distortion_params[0]:
-            image = self.elastic_distortion(image, *self.elastic_distortion_params[1:])
+        # # Elastic Distortions
+        # if self.elastic_distortion_params and np.random.random() < self.elastic_distortion_params[0]:
+        #     image = self.elastic_distortion(image, *self.elastic_distortion_params[1:])
 
         # # Perspective Transforms
         # if self.perspective_transform_params and np.random.random() < self.perspective_transform_params[0]:
         #     image = self.perspective_transform(image, *self.perspective_transform_params[1:])
 
         # # Gaussian Noise
-        # if self.gaussian_noise_params and np.random.random() < self.gaussian_noise_params[0]:
-        #     image = self.gaussian_noise(image, *self.gaussian_noise_params[1:])
+        # if self.salt_and_pepper_params and np.random.random() < self.salt_and_pepper_params[0]:
+        #     image = self.salt_and_pepper(image, *self.salt_and_pepper_params[1:])
 
-        # # Gaussian Blur
-        # if self.gaussian_blur_params and np.random.random() < self.gaussian_blur_params[0]:
-        #     image = self.gaussian_blur(image, *self.gaussian_blur_params[1:])
+        # Gaussian Blur
+        if self.gaussian_blur_params and np.random.random() < self.gaussian_blur_params[0]:
+            image = self.gaussian_blur(image, *self.gaussian_blur_params[1:])
 
         # # Shearing
         # if self.shearing_params and np.random.random() < self.shearing_params[0]:
@@ -188,7 +234,7 @@ class Augmentor():
         # if self.translate_y_params and np.random.random() < self.translate_y_params[0]:
         #     image = self.translate_y(image, *self.translate_y_params[1:])
 
-        return np.array(image)
+        return image
 
     def mixup(self, image, batch_images, opacity, pickups, radius=True):
         """
@@ -230,7 +276,7 @@ class Augmentor():
 
             image = cv2.addWeighted(image, 1 - pickup_opac, pickup_img, pickup_opac, 0)
 
-        return np.array(image)
+        return image
 
     def erosion(self, image, kernel_size, iterations, radius=True):
         """
@@ -254,13 +300,13 @@ class Augmentor():
         """
 
         if radius:
-            kernel_size = np.random.randint(1, kernel_size + 1)
-            iterations = np.random.randint(0, iterations + 1)
+            kernel_size = np.random.randint(1, max(1, kernel_size) + 1)
+            iterations = np.random.randint(1, max(1, iterations) + 1)
 
         kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
         image = cv2.erode(image, kernel, iterations=iterations)
 
-        return np.array(image)
+        return image
 
     def dilation(self, image, kernel_size, iterations, radius=True):
         """
@@ -284,13 +330,13 @@ class Augmentor():
         """
 
         if radius:
-            kernel_size = np.random.randint(1, kernel_size + 1)
-            iterations = np.random.randint(0, iterations + 1)
+            kernel_size = np.random.randint(1, max(1, kernel_size) + 1)
+            iterations = np.random.randint(1, max(1, iterations) + 1)
 
         kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
         image = cv2.dilate(image, kernel, iterations=iterations)
 
-        return np.array(image)
+        return image
 
     def elastic_distortion(self, image, grid_size, magnitude, radius=True):
         """
@@ -314,8 +360,8 @@ class Augmentor():
         """
 
         if radius:
-            grid_size = np.random.randint(1, grid_size + 1)
-            magnitude = np.random.randint(1, magnitude + 1)
+            grid_size = np.random.randint(1, max(1, grid_size) + 1)
+            magnitude = np.random.randint(1, max(1, magnitude) + 1)
 
         height, width = image.shape[:2]
 
@@ -371,14 +417,17 @@ class Augmentor():
             x1, y1, x2, y2, x3, y3, x4, y4 = polygons[d]
             polygons[d] = [x1 + dx, y1 + dy, x2, y2, x3, y3, x4, y4]
 
-        mesh = [[dimensions[i], polygons[i]] for i in range(len(dimensions))]
+        generated_mesh = [[dimensions[i], polygons[i]] for i in range(len(dimensions))]
+        background_color = int(np.bincount(np.ravel(image)).argmax())
 
-        image = Image.fromarray(image)
-        image = image.transform(image.size, Image.MESH, mesh, resample=Image.BICUBIC, fillcolor=255)
+        image = Image.fromarray(image).transform(image.shape[::-1], Image.MESH,
+                                                 data=generated_mesh,
+                                                 resample=Image.BICUBIC,
+                                                 fillcolor=background_color)
 
         return np.array(image)
 
-    def perspective_transform(self, image, type, magnitude, radius=True):
+    def perspective_transform(self, image, magnitude, radius=True):
         """
         Apply perspective transform to the image.
 
@@ -386,8 +435,6 @@ class Augmentor():
         ----------
         image : ndarray
             Input image to be transformed.
-        type : int
-            Type of perspective transform.
         magnitude : float
             Magnitude of perspective transform.
         radius : bool, optional
@@ -399,12 +446,34 @@ class Augmentor():
             Transformed image.
         """
 
-        # do something
-        # ...
+        if radius:
+            magnitude = np.random.uniform(1e-8, max(1e-8, magnitude))
 
-        return np.array(image)
+        height, width = image.shape[:2]
+        max_offset = int(min(height, width) * magnitude)
 
-    def gaussian_noise(self, image, kernel_size, iterations, radius=True):
+        src_points = np.array([
+            (0, 0),
+            (width - 1, 0),
+            (width - 1, height - 1),
+            (0, height - 1),
+        ], dtype=np.float32)
+
+        dst_points = np.array([
+            (np.random.randint(0, max_offset), np.random.randint(0, max_offset)),
+            (np.random.randint(width - max_offset, width), np.random.randint(0, max_offset)),
+            (np.random.randint(width - max_offset, width), np.random.randint(height - max_offset, height)),
+            (np.random.randint(0, max_offset), np.random.randint(height - max_offset, height)),
+        ], dtype=np.float32)
+
+        M = cv2.getPerspectiveTransform(src_points, dst_points)
+
+        background_color = int(np.bincount(np.ravel(image)).argmax())
+        image = cv2.warpPerspective(image, M, image.shape[::-1], borderValue=background_color)
+
+        return image
+
+    def salt_and_pepper(self, image, noise_percentage, radius=True):
         """
         Apply Gaussian noise to the image.
 
@@ -412,12 +481,10 @@ class Augmentor():
         ----------
         image : ndarray
             Input image to be noised.
-        kernel_size : int
-            Kernel size for Gaussian noise.
-        iterations : int
-            Number of iterations for Gaussian noise.
+        noise_percentage : float
+            Percentage of pixels to add noise to (between 0 and 1).
         radius : bool, optional
-            Whether to use range radius for kernel size and iterations, by default True.
+            Whether to use range radius for noise_percentage, by default True.
 
         Returns
         -------
@@ -425,10 +492,24 @@ class Augmentor():
             Noised image.
         """
 
-        # do something
-        # ...
+        if radius:
+            noise_percentage = np.random.uniform(1e-8, max(1e-8, noise_percentage))
 
-        return np.array(image)
+        num_pixels = image.size
+        num_pixels_to_noise = int(num_pixels * noise_percentage)
+
+        indices = np.random.choice(num_pixels, num_pixels_to_noise, replace=False)
+
+        salt_mask = np.random.rand(num_pixels_to_noise) < 0.5
+        pepper_mask = ~salt_mask
+
+        noisy_image = image.flatten()
+        noisy_image[indices[salt_mask]] = 255
+        noisy_image[indices[pepper_mask]] = 0
+
+        image = noisy_image.reshape(image.shape)
+
+        return image
 
     def gaussian_blur(self, image, kernel_size, iterations, radius=True):
         """
@@ -451,10 +532,14 @@ class Augmentor():
             Blurred image.
         """
 
-        # do something
-        # ...
+        if radius:
+            kernel_size = np.random.choice(range(1, max(1, kernel_size) + 1, 2))
+            iterations = np.random.randint(1, max(1, iterations) + 1)
 
-        return np.array(image)
+        for _ in range(iterations):
+            image = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+
+        return image
 
     def shearing(self, image, value, radius=True):
         """
@@ -478,7 +563,7 @@ class Augmentor():
         # do something
         # ...
 
-        return np.array(image)
+        return image
 
     def scaling(self, image, value, radius=True):
         """
@@ -502,7 +587,7 @@ class Augmentor():
         # do something
         # ...
 
-        return np.array(image)
+        return image
 
     def rotation(self, image, value, radius=True):
         """
@@ -526,7 +611,7 @@ class Augmentor():
         # do something
         # ...
 
-        return np.array(image)
+        return image
 
     def translate_x(self, image, value, radius=True):
         """
@@ -550,7 +635,7 @@ class Augmentor():
         # do something
         # ...
 
-        return np.array(image)
+        return image
 
     def translate_y(self, image, value, radius=True):
         """
@@ -574,4 +659,4 @@ class Augmentor():
         # do something
         # ...
 
-        return np.array(image)
+        return image
