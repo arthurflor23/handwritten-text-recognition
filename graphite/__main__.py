f6969218f9178348580c1ba832a0a74b0de6ccdf
Arthur Flôr <arthurflor23@gmail.com>
2023-06-01 19:38:32 -0300

    Update main script

--- graphite/__main__.py
+++ graphite/__main__.py
@@ -11,11 +11,13 @@ if __name__ == '__main__':
 
     parser.add_argument('--test', default=False, action='store_true',
                         help="Perform optical model test")
+
     parser.add_argument('--train', default=False, action='store_true',
                         help="Perform optical model training")
 
     parser.add_argument('--check', default=False, action='store_true',
                         help="Perform data verification")
+
     parser.add_argument('--check-samples', default=False, action='store_true',
                         help="View sample data")
 
@@ -26,52 +28,70 @@ if __name__ == '__main__':
     # Dataset
     parser.add_argument('--source', default=None,
                         help="Define the source data (iam, rimes)")
+
     parser.add_argument('--level', default='line',
                         help="Define the recoginition level (line, paragraph)")
+
     parser.add_argument('--training-ratio', default=None,
                         help="Set the training partition ratio")
+
     parser.add_argument('--validation-ratio', default=None,
                         help="Set the validation partition ratio")
+
     parser.add_argument('--test-ratio', default=None,
                         help="Set the test partition ratio")
+
     parser.add_argument('--lazy-mode', default=False, action='store_true',
                         help="Enable lazy loading")
 
     # Data augmentation
-    parser.add_argument('--erosion', default=[0.99, 5, 2], nargs='+', type=float,
+    parser.add_argument('--erosion', default=[0.99, 9, 1], nargs='+', type=float,
                         help="Apply dilation (probability, kernel_size, iterations)")
+
     parser.add_argument('--dilation', default=[0.99, 3, 1], nargs='+', type=float,
                         help="Apply dilation (probability, kernel_size, iterations)")
-    parser.add_argument('--elastic-transform', default=[0.99, 33], nargs='+', type=float,
+
+    parser.add_argument('--elastic-transform', default=[0.99, 43], nargs='+', type=float,
                         help="Apply elastic transform (probability, kernel size)")
-    parser.add_argument('--mixup', default=[0.99, 0.2, 1], nargs='+', type=float,
+
+    parser.add_argument('--mixup', default=[0.99, 0.3, 1], nargs='+', type=float,
                         help="Apply mixup augmentation (probability, opacity, pickups)")
-    parser.add_argument('--perspective-transform', default=[0.99, 0.5], nargs='+', type=float,
+
+    parser.add_argument('--perspective-transform', default=[0.99, 0.15], nargs='+', type=float,
                         help="Apply perspective transformation (probability, factor)")
-    parser.add_argument('--salt-and-pepper', default=[0.99, 0.1], nargs='+', type=float,
+
+    parser.add_argument('--salt-and-pepper', default=[0.99, 0.3], nargs='+', type=float,
                         help="Apply Gaussian noise (probability, percentage)")
-    parser.add_argument('--gaussian-blur', default=[0.99, 3, 5], nargs='+', type=float,
+
+    parser.add_argument('--gaussian-blur', default=[0.99, 9, 1], nargs='+', type=float,
                         help="Apply Gaussian blur (probability, kernel size, iterations)")
-    parser.add_argument('--shearing', default=[0.99, 1.5], nargs='+', type=float,
+
+    parser.add_argument('--shearing', default=[0.99, 0.15], nargs='+', type=float,
                         help="Apply shearing transformation (probability, factor)")
-    parser.add_argument('--scaling', default=[0.99, 0.75, 1.0], nargs='+', type=float,
-                        help="Apply scaling transformation (probability, min_factor, max_factor)")
-    parser.add_argument('--rotation', default=[0.99, 5], nargs='+', type=float,
+
+    parser.add_argument('--scaling', default=[0.99, 0.25], nargs='+', type=float,
+                        help="Apply scaling transformation (probability, factor)")
+
+    parser.add_argument('--rotation', default=[0.99, 1.5], nargs='+', type=float,
                         help="Apply rotation transformation (probability, angle)")
-    parser.add_argument('--translation', default=[0.99, 0.5, 0.5], nargs='+', type=float,
+
+    parser.add_argument('--translation', default=[0.99, 0.15, 0.15], nargs='+', type=float,
                         help="Apply vertical and horizontal translation (probability, y_factor, x_factor)")
+
     parser.add_argument('--disable-augmentation', default=True, action='store_false',
                         help="Disable data augmentation completely")
 
     # Inference
     parser.add_argument('--images', default=[], nargs='+',
                         help="Set image path list for handwriting recognition")
+
     parser.add_argument('--bbox', default=[], nargs='+',
                         help="Set bounding box values (x, y, width, height)")
 
     # Spell check
     parser.add_argument('--spell-check', default='openai',
                         help="Define the spell check (openai)")
+
     parser.add_argument('--api-key', default=None,
                         help="Set the spell check API_KEY")
 
