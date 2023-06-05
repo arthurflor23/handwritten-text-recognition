f49f8a496cb8a85bcdfc9a1e332772720cfeb771
Arthur Flôr <arthurflor23@gmail.com>
2023-06-05 20:17:25 -0300

    Add initial training script

--- graphite/task/train.py
+++ graphite/task/train.py
@@ -1,3 +1,37 @@
+from dataset import Dataset
+from model import OpticalModel
+from util import Augmentor, Carbon, SpellChecker
+
 
 def train(args):
-    print("train", args)
+
+    dataset = Dataset(source=args.source,
+                      level=args.level,
+                      training_ratio=args.training_ratio,
+                      validation_ratio=args.validation_ratio,
+                      test_ratio=args.test_ratio,
+                      lazy_mode=args.lazy_mode,
+                      seed=42)
+
+    augmentor = Augmentor(erosion=args.erosion,
+                          dilation=args.dilation,
+                          elastic_transform=args.elastic_transform,
+                          mixup=args.mixup,
+                          perspective_transform=args.perspective_transform,
+                          salt_and_pepper=args.salt_and_pepper,
+                          gaussian_blur=args.gaussian_blur,
+                          shearing=args.shearing,
+                          scaling=args.scaling,
+                          rotation=args.rotation,
+                          translation=args.translation,
+                          reference_pixels=dataset.reference_pixels,
+                          seed=42)
+
+    spell_checker = SpellChecker(env_key='OPENAI_API_KEY')
+
+    optical_model = OpticalModel()
+
+    # carbon = Carbon(dataset=dataset,
+    #                 augmentor=augmentor,
+    #                 spell_checker=spell_checker,
+    #                 optical_model=optical_model)
