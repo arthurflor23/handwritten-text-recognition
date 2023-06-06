cf9f70c2053f9fc32b8678942d95a85b2f01e7a4
Arthur Flôr <arthurflor23@gmail.com>
2023-06-05 21:18:56 -0300

    Initial model class

--- graphite/model/model.py
+++ graphite/model/model.py
@@ -1,5 +1,42 @@
+import os
+import importlib
+import tensorflow as tf
+
 
 class OpticalModel():
+    """
+    General optical model management.
+    """
 
-    def __init__(self):
-        print("OpticalModel self...")
+    def __init__(self,
+                 network=None,
+                 data_path='mlruns',
+                 run_id=None,
+                 seed=None):
+
+        tf.random.set_seed(seed)
+
+        self.network = network
+        self.base_path = os.path.join(os.path.dirname(__file__), '..', '..')
+        self.data_path = os.path.join(self.base_path, data_path)
+        self.run_id = run_id
+        self.infer = run_id is not None
+        self.seed = seed
+
+        if not self.infer:
+            self.network_class = self._get_network_class()
+
+    def _get_network_class(self):
+
+        module_name = importlib.util.resolve_name(f".network.{self.network}", __package__)
+        module_spec = importlib.util.find_spec(module_name)
+        assert module_spec is not None, "network file must be created"
+
+        module = importlib.import_module(module_name, __package__)
+
+        class_name = 'Network'
+        assert hasattr(module, class_name), f"`{class_name}` class must be created"
+
+        network_class = getattr(module, class_name)()
+
+        return network_class
