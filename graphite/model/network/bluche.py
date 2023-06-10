e933e7b9150b080b9aff6ab392a6b8b4f058aa47
Arthur Flôr <arthurflor23@gmail.com>
2023-06-09 21:40:58 -0300

    Update bluche script

--- graphite/model/network/bluche.py
+++ graphite/model/network/bluche.py
@@ -5,24 +5,22 @@ from .layers import GatedConv2D
 
 class Network():
 
-    def compile_model(self,
-                      output_size,
-                      learning_rate,
-                      ctc_loss_func):
+    def compile_model(self, output_shape, learning_rate, ctc_loss_func):
 
-        inputs, outputs = self._get_architecture(input_size=(1024, 128, 1), output_size=output_size)
+        inputs, outputs = self._get_architecture(output_shape)
         model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-        optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
+        optimizer = tf.keras.optimizers.legacy.RMSprop(learning_rate=learning_rate)
         model.compile(optimizer=optimizer, loss=ctc_loss_func)
 
         return model
 
-    def _get_architecture(self, input_size, output_size):
+    def _get_architecture(self, output_shape):
 
-        inputs = tf.keras.layers.Input(name='input', shape=input_size)
+        input_shape = (1024, 128, 1)
+        inputs = tf.keras.layers.Input(name='input', shape=input_shape)
 
-        target_shape = (input_size[0] // 2, input_size[1] // 2, input_size[2] * 4)
+        target_shape = (input_shape[0] // 2, input_shape[1] // 2, input_shape[2] * 4)
         cnn = tf.keras.layers.Reshape(target_shape=target_shape)(inputs)
 
         cnn = tf.keras.layers.Conv2D(filters=8,
@@ -63,6 +61,8 @@ class Network():
 
         cnn = tf.keras.layers.MaxPooling2D(pool_size=(1, 4), strides=(1, 4), padding='valid')(cnn)
 
+        print(output_shape)
+
         shape = cnn.get_shape()
         blstm = tf.keras.layers.Reshape((shape[1], shape[2] * shape[3]))(cnn)
 
@@ -70,6 +70,6 @@ class Network():
         blstm = tf.keras.layers.Dense(units=128, activation='tanh')(blstm)
 
         blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=128, return_sequences=True))(blstm)
-        outputs = tf.keras.layers.Dense(units=output_size, activation='softmax')(blstm)
+        outputs = tf.keras.layers.Dense(units=output_shape[-1], activation='softmax')(blstm)
 
         return (inputs, outputs)
