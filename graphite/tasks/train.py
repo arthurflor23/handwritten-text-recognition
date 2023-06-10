816be3722f5af6425802fe12a3c5cdcb43afcaff
Arthur Flôr <arthurflor23@gmail.com>
2023-06-09 21:19:05 -0300

    Update train script

--- graphite/tasks/train.py
+++ graphite/tasks/train.py
@@ -14,29 +14,31 @@ def train(args):
                       lazy_mode=args.lazy_mode,
                       seed=42)
 
-    # augmentor = Augmentor(erosion=args.erosion,
-    #                       dilation=args.dilation,
-    #                       elastic_transform=args.elastic_transform,
-    #                       mixup=args.mixup,
-    #                       perspective_transform=args.perspective_transform,
-    #                       salt_and_pepper=args.salt_and_pepper,
-    #                       gaussian_blur=args.gaussian_blur,
-    #                       shearing=args.shearing,
-    #                       scaling=args.scaling,
-    #                       rotation=args.rotation,
-    #                       translation=args.translation,
-    #                       reference_pixels=dataset.reference_pixels,
-    #                       seed=42)
+    augmentor = Augmentor(erosion=args.erosion,
+                          dilation=args.dilation,
+                          elastic_transform=args.elastic_transform,
+                          mixup=args.mixup,
+                          perspective_transform=args.perspective_transform,
+                          salt_and_pepper=args.salt_and_pepper,
+                          gaussian_blur=args.gaussian_blur,
+                          shearing=args.shearing,
+                          scaling=args.scaling,
+                          rotation=args.rotation,
+                          translation=args.translation,
+                          reference_pixels=dataset.reference_pixels,
+                          disable_augmentation=args.disable_augmentation,
+                          seed=42)
 
-    # spelling = Spelling(spell_checker=args.spell_checker,
-    #                     api_key=args.api_key,
-    #                     env_key=args.env_key)
+    spelling = Spelling(spell_checker=args.spell_checker, api_key=args.api_key, env_key=args.env_key)
 
-    print(dataset.tokenizer.shape)
+    model = Model(network=args.network, tokenizer=dataset.tokenizer, seed=42)
+    model.compile(learning_rate=args.learning_rate, model_uri=None)
 
-    model = Model(network=args.network,
-                  network_flavor=args.network_flavor,
-                  seed=42)
+    # model.fit(...,
+    #           plateau_cooldown=args.plateau_cooldown,
+    #           plateau_factor=args.plateau_factor,
+    #           plateau_patience=args.plateau_patience,
+    #           patience=args.patience)
 
     # carbon = Carbon(dataset=dataset,
     #                 augmentor=augmentor,
