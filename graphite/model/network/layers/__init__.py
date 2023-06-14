f24e2675a1a829b58e4ceeb16d0b8e2bd744cf0a
Arthur Flôr <arthurflor23@gmail.com>
2023-06-13 23:33:39 -0300

    Add Processing module

--- graphite/model/network/layers/__init__.py
+++ graphite/model/network/layers/__init__.py
@@ -1,5 +1,6 @@
 from .FullGatedConv2D import FullGatedConv2D
 from .GatedConv2D import GatedConv2D
 from .OctConv2D import OctConv2D
+from .Processing import Processing
 
-__all__ = ['FullGatedConv2D', 'GatedConv2D', 'OctConv2D']
+__all__ = ['FullGatedConv2D', 'GatedConv2D', 'OctConv2D', 'Processing']
