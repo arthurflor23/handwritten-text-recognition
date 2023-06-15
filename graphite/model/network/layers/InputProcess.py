b78682db73120c3d119d36d01a6692398e41af41
Arthur Flôr <arthurflor23@gmail.com>
2023-06-15 08:59:19 -0300

    Add optional resizing

--- graphite/model/network/layers/InputProcess.py
+++ graphite/model/network/layers/InputProcess.py
@@ -6,14 +6,14 @@ class InputProcess(tf.keras.layers.Layer):
     This class defines a custom layer for processing.
     """
 
-    def __init__(self, target_shape, **kwargs):
+    def __init__(self, target_shape=None, **kwargs):
         """
         Initialize the InputProcess class.
 
         Parameters
         ----------
         target_shape : int
-            Target shape for resizing operation.
+            Target shape for resizing operation. Default is None.
         **kwargs :
             Variable length argument list for parent class.
         """
@@ -37,14 +37,15 @@ class InputProcess(tf.keras.layers.Layer):
             Processed tensor after applying operations.
         """
 
-        inputs = tf.image.resize_with_pad(inputs,
-                                          target_height=self.target_shape[1],
-                                          target_width=self.target_shape[0],
-                                          method=tf.image.ResizeMethod.BILINEAR,
-                                          antialias=True)
-
+        inputs = tf.math.divide(inputs, 255.)
         inputs = tf.image.transpose(inputs)
-        inputs = tf.math.divide(inputs, 255)
+
+        if self.target_shape is not None:
+            inputs = tf.math.subtract(1., tf.image.resize_with_pad(image=tf.math.subtract(1., inputs),
+                                                                   target_height=self.target_shape[0],
+                                                                   target_width=self.target_shape[1],
+                                                                   method=tf.image.ResizeMethod.BICUBIC,
+                                                                   antialias=True))
 
         return inputs
 
