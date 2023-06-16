aa7a97958271158789d9b8efe986913c164e8ed9
Arthur Flôr <arthurflor23@gmail.com>
2023-06-16 08:03:05 -0300

    Update check script

--- graphite/tasks/check.py
+++ graphite/tasks/check.py
@@ -27,9 +27,9 @@ def check(args):
                       seed=42)
     print(dataset)
 
-    augmentor = Augmentor(erosion=args.erosion,
+    augmentor = Augmentor(elastic_transform=args.elastic_transform,
+                          erosion=args.erosion,
                           dilation=args.dilation,
-                          elastic_transform=args.elastic_transform,
                           mixup=args.mixup,
                           perspective_transform=args.perspective_transform,
                           salt_and_pepper=args.salt_and_pepper,
