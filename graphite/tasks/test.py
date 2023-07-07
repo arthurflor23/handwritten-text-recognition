c66f2b8f24fd29923ca1fdd17c61afa5beb5548e
Arthur Flôr <arthurflor23@gmail.com>
2023-07-07 13:00:19 -0300

    Update test script

--- graphite/tasks/test.py
+++ graphite/tasks/test.py
@@ -16,7 +16,7 @@ def test(args):
     """
 
     def print_section(content):
-        print(f"\n{'=' * 65}\n{content}\n{'=' * 65}\n")
+        print(f"\n{'=' * 72}\n{content}\n{'=' * 72}\n")
 
     dataset = Dataset(source=args.source,
                       level=args.level,
