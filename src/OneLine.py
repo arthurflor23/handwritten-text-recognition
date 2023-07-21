33c9aac33922c64fc81bb02abd63bc321cad139e
Lance Parrish <lanciferp@gmail.com>
2023-07-21 11:17:39 -0400

    Update OneLine.py

--- src/OneLine.py
+++ src/OneLine.py
@@ -12,7 +12,7 @@ def main():
     args = parser.parse_args()
 
     # Read in YAML file
-    yaml_path = os.path.join(".", "job_config", args.job_config + ".yaml")
+    yaml_path = os.path.join(".", "job_config", args.job_config)
     with open(yaml_path, "r") as f:
         job_config = yaml.safe_load(f)
 
@@ -33,7 +33,9 @@ def main():
             os.makedirs(output_path, exist_ok=True)
 
             for model in model_list:
-                model_output_path = os.path.join(output_path, str(year), sub_name, column_name, model)
+                model_output_name = model + '_' + column_name + '.csv'
+                model_output_path = os.path.join(output_path, str(year), sub_name, column_name,
+                                                 model, model_output_name)
                 command = "sbatch run_batch.sh " + " ".join((input_path, model, model_output_path))
                 os.system(command)
 
