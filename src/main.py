9d8f30e3ea895312dfdbfe461ccf3b2e81ac4124
Lance Parrish <lanciferp@gmail.com>
2023-07-24 19:17:35 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -261,7 +261,7 @@ if __name__ == "__main__":
             predicted_blank = blank_detector.predictBlank(img)
 
             img = pp.preprocess(image_path, input_size=input_size)
-            # x_test = pp.normalization([img])
+            x_test = pp.normalization([img])
             # predicts, probabilities = model.predict(x_test, ctc_decode=True)
             # predicts = [[tokenizer.decode(x) for x in y] for y in predicts]
             # final_predicts.append([image_name, predicts[0][0], probabilities[0][0], predicted_blank])
