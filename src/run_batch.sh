c95c5c5e0fa96484676e046d490a7c315ff20006
Lance Parrish <lanciferp@gmail.com>
2023-07-26 14:06:41 -0400

    Update run_batch.sh

--- src/run_batch.sh
+++ src/run_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=4   # number of processor cores (i.e., tasks)
-#SBATCH --mem=5G   # memory per CPU core
+#SBATCH --cpus-per-task=2   # number of processor cores (i.e., tasks)
+#SBATCH --mem=3G   # memory per CPU core
 #SBATCH -J "Flor-Batch"   # job name
 #SBATCH -e ./flor_out/%j-err.txt
 #SBATCH -o ./flor_out/%j-out.txt
