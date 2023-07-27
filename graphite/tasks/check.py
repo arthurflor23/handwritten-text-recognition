d4ba208f8c5b95918daf357ab4da8178c6fa6bf9
Arthur Flôr <arthurflor23@gmail.com>
2023-07-26 22:31:08 -0300

    Update check script

--- graphite/tasks/check.py
+++ graphite/tasks/check.py
@@ -40,7 +40,6 @@ def check(args):
                           salt_and_pepper=args.salt_and_pepper,
                           gaussian_noise=args.gaussian_noise,
                           gaussian_blur=args.gaussian_blur,
-                          shuffle=(not args.sequential_augmentation),
                           seed=args.seed)
     print(augmentor)
 
