a00d615584dfd5de867e20eac38070d16199d74a
Arthur Flor <arthurflor23@gmail.com>
2023-10-16 19:04:53 -0300

    Update main script

--- graphite/__main__.py
+++ graphite/__main__.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
 
     # Dataset
     parser.add_argument('--source', default=None, help='Define source data')
-    parser.add_argument('--level', default='line', help='Define text structure level')
-    parser.add_argument('--shape', default=[128, 1024, 1], nargs='+', type=int, help='Define image shape')
+    parser.add_argument('--text-level', default='line', help='Define text structure level')
+    parser.add_argument('--image-shape', default=[128, 1024, 1], nargs=3, type=int, help='Define image shape (HxWxC)')
     parser.add_argument('--training-ratio', default=None, help='Define training partition ratio')
     parser.add_argument('--validation-ratio', default=None, help='Define validation partition ratio')
     parser.add_argument('--test-ratio', default=None, help='Define test partition ratio')
@@ -27,19 +27,19 @@ if __name__ == '__main__':
     parser.add_argument('--lazy-mode', default=False, action='store_true', help='Enable lazy loading mode')
 
     # Augmentor
-    parser.add_argument('--erode', default=[0.66, 3, 1], nargs='+', type=float, help='Erosion parameters')
-    parser.add_argument('--dilate', default=[0.33, 2, 1], nargs='+', type=float, help='Dilation parameters')
-    parser.add_argument('--elastic', default=[0.66, 29, 1.0], nargs='+', type=float, help='Elastic parameters')
-    parser.add_argument('--perspective', default=[0.66, 0.4], nargs='+', type=float, help='Perspective parameters')
-    parser.add_argument('--mixup', default=None, nargs='+', type=float, help='Mixup parameters')
-    parser.add_argument('--shear', default=[0.33, 15], nargs='+', type=float, help='Shearing parameters')
-    parser.add_argument('--scale', default=[0.33, 0.1], nargs='+', type=float, help='Scaling parameters')
-    parser.add_argument('--rotate', default=[0.33, 1.0], nargs='+', type=float, help='Rotation parameters')
-    parser.add_argument('--shift-y', default=None, nargs='+', type=float, help='Vertical translation parameters')
-    parser.add_argument('--shift-x', default=None, nargs='+', type=float, help='Horizontal translation parameters')
-    parser.add_argument('--salt-and-pepper', default=None, nargs='+', type=float, help='Salt & pepper parameters')
-    parser.add_argument('--gaussian-noise', default=None, nargs='+', type=float, help='Gaussian noise parameters')
-    parser.add_argument('--gaussian-blur', default=None, nargs='+', type=float, help='Gaussian blur parameters')
+    parser.add_argument('--erode', default=[0.66, 3, 1], nargs=3, type=float, help='Erosion parameters')
+    parser.add_argument('--dilate', default=[0.33, 2, 1], nargs=3, type=float, help='Dilation parameters')
+    parser.add_argument('--elastic', default=[0.66, 29, 1.0], nargs=3, type=float, help='Elastic parameters')
+    parser.add_argument('--perspective', default=[0.66, 0.4], nargs=2, type=float, help='Perspective parameters')
+    parser.add_argument('--mixup', default=None, nargs=3, type=float, help='Mixup parameters')
+    parser.add_argument('--shear', default=[0.33, 15], nargs=2, type=float, help='Shearing parameters')
+    parser.add_argument('--scale', default=[0.33, 0.1], nargs=2, type=float, help='Scaling parameters')
+    parser.add_argument('--rotate', default=[0.33, 1.0], nargs=2, type=float, help='Rotation parameters')
+    parser.add_argument('--shift-y', default=None, nargs=2, type=float, help='Vertical translation parameters')
+    parser.add_argument('--shift-x', default=None, nargs=2, type=float, help='Horizontal translation parameters')
+    parser.add_argument('--salt-and-pepper', default=None, nargs=2, type=float, help='Salt & pepper parameters')
+    parser.add_argument('--gaussian-noise', default=None, nargs=2, type=float, help='Gaussian noise parameters')
+    parser.add_argument('--gaussian-blur', default=None, nargs=3, type=float, help='Gaussian blur parameters')
     parser.add_argument('--disable-augmentation', default=False, action='store_true', help='Disable all augmentations')
 
     # Training
