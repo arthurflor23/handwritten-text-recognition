ca25ce8314bf0c99ec827f2fd34798c5e68c6a2e
Arthur Flor <arthurflor23@gmail.com>
2023-11-17 15:47:52 -0300

    Update references

--- graphite/__init__.py
+++ graphite/__init__.py
@@ -1,6 +1,6 @@
-# from graphite.dataset import Augmentor
-# from graphite.dataset import Dataset
-# from graphite.model import Model
+# from graphite.datasets import Augmentor
+# from graphite.datasets import Dataset
+# from graphite.models import Model
 # from graphite.spelling import Spelling
 
 # __all__ = ['Augmentor', 'Dataset', 'Model', 'Spelling']
