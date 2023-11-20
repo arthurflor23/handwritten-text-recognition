fa51d3c1e0587a94bfa92ee2d008d12113cf6a5f
Arthur Flor <arthurflor23@gmail.com>
2023-11-19 21:36:35 -0300

    Add DynamicReshape reference

--- graphite/layers/__init__.py
+++ graphite/layers/__init__.py
@@ -1,5 +1,6 @@
 from layers.attention import SpectralSelfAttention
 from layers.normalization import ConditionalBatchNormalization
 from layers.normalization import SpectralNormalization
+from layers.processing import DynamicReshape
 
-__all__ = ['SpectralSelfAttention', 'ConditionalBatchNormalization', 'SpectralNormalization']
+__all__ = ['SpectralSelfAttention', 'ConditionalBatchNormalization', 'SpectralNormalization', 'DynamicReshape']
