239e1d0475224d6a7d423852c501477825386d9a
Arthur Flor <arthurflor23@gmail.com>
2023-11-19 21:49:03 -0300

    Update generator model

--- graphite/models/synthesis/higanplus.py
+++ graphite/models/synthesis/higanplus.py
@@ -1,155 +1,228 @@
 import tensorflow as tf
 
-from layers import SpectralSelfAttention, ConditionalBatchNormalization
+from layers import SpectralSelfAttention
+from layers import ConditionalBatchNormalization
+from layers import SpectralNormalization
+from layers import DynamicReshape
 
 
 class SynthesisModel(tf.keras.Model):
 
-    def __init__(self,
-                 vocab_size=80,
-                 embedding_dim=120,
-                 style_dim=32):
+    def __init__(self, image_shape, latent_dim, vocab_dim, embedding_dim, **kwargs):
+        super().__init__(**kwargs)
 
-        super(SynthesisModel, self).__init__()
+        self.generator = GeneratorModel(image_shape, latent_dim, vocab_dim, embedding_dim)
+        self.generator.summary()
 
-        self.generator = GeneratorModule(vocab_size, embedding_dim, style_dim)
-        self.discriminator = None
-        self.patch_discriminator = None
-        self.style_encoder = None
-        self.style_backbone = None
-        self.writer_identifier = None
-        self.recognizer = None
+        # self.generator = GeneratorModule(vocab_dim)
+        # self.discriminator = None
+        # self.patch_discriminator = None
+        # self.style_encoder = None
+        # self.style_backbone = None
+        # self.writer_identifier = None
+        # self.recognizer = None
 
     def compile(self, learning_rate=None):
-        super(SynthesisModel, self).compile()
+        super().compile(run_eagerly=False)
 
         self.optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate)
 
+        # self.generator.compile(loss='mse', optimizer=self.optimizer)
+
     def train_step(self, data):
-        (image_inputs, text_inputs), _ = data
+        (image_inputs, texv_inputs), _ = data
 
-        with tf.GradientTape() as tape:
-            # Generate images
-            generated_images = self.generator(image_inputs, text_inputs)
+    #     with tf.GradientTape() as tape:
+    #         # Generate images
+    #         generated_images = self.generator(image_inputs, texv_inputs)
 
-            # Get the dynamic shape of the generated images
-            dynamic_shape = tf.shape(generated_images)
+    #         # Get the dynamic shape of the generated images
+    #         dynamic_shape = tf.shape(generated_images)
 
-            # Create dummy target data for loss calculation with the dynamic shape
-            target = tf.random.normal(dynamic_shape, dtype=generated_images.dtype)
+    #         # Create dummy target data for loss calculation with the dynamic shape
+    #         target = tf.random.normal(dynamic_shape, dtype=generated_images.dtype)
 
-            # Simple loss function (mean squared error)
-            loss = tf.reduce_mean(tf.square(generated_images - target))
+    #         # Simple loss function (mean squared error)
+    #         loss = tf.reduce_mean(tf.square(generated_images - target))
 
-        # Calculate gradients and update model weights
-        gradients = tape.gradient(loss, self.generator.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.generator.trainable_variables))
+    #     # Calculate gradients and update model weights
+    #     gradients = tape.gradient(loss, self.generator.trainable_variables)
+    #     self.optimizer.apply_gradients(zip(gradients, self.generator.trainable_variables))
 
-        return {"loss": loss}
-
-
-class GeneratorModule(tf.keras.layers.Layer):
-
-    def __init__(self,
-                 vocab_size,
-                 embedding_dim,
-                 style_dim):
-
-        super(GeneratorModule, self).__init__()
-
-        self.text_embedding = tf.keras.layers.Embedding(
-            input_dim=vocab_size + 1, output_dim=embedding_dim, mask_zero=True)
-
-        # self.filter_sn_dense = tf.keras.layers.SpectralNormalization(
-        #     tf.keras.layers.Dense(units=embedding_dim + style_dim), power_iterations=512 * 16)
-
-        # self.style_sn_dense = tf.keras.layers.SpectralNormalization(
-        #     tf.keras.layers.Dense(units=style_dim), power_iterations=style_dim * 4)
-
-        # # blocks
-        # self.block1 = GeneratorBlock(units=256, upsample=(2, 1))
-        # self.block2 = GeneratorBlock(units=128, upsample=(2, 2))
-        # self.block3 = GeneratorBlock(units=64, upsample=(2, 2))
-        # self.block4 = GeneratorBlock(units=64, upsample=(2, 2))
-
-        # self.attention = SpectralSelfAttention(units=64)
-
-        # self.outlayer = tf.keras.Sequential([
-        #     tf.keras.layers.BatchNormalization(),
-        #     # ConditionalBatchNormalization(units=64),
-        #     tf.keras.layers.ReLU(),
-        #     tf.keras.layers.SpectralNormalization(
-        #         tf.keras.layers.Conv2D(filters=1, kernel_size=3, padding='same'))
-        # ])
-
-        self.conv = tf.keras.layers.Conv2D(filters=1, kernel_size=3, padding='same')
-
-    def call(self, image_inputs, text_inputs, training=True):
-
-        # # Process text inputs through the embedding layer
-        # text_embedding = self.text_embedding(text_inputs)
-
-        # # Filter processing
-        # filter_processed = self.filter_sn_dense(text_embedding)
-
-        # # Style processing (if needed, depending on how style is derived from text)
-        # style_processed = self.style_sn_dense(text_embedding)
-
-        # # Combine the filter and style processed outputs with the image inputs
-        # x = tf.concat([image_inputs, filter_processed, style_processed], axis=-1)
-
-        # # Passing through Generator Blocks
-        # x = self.block1(x, training=training)
-        # x = self.block2(x, training=training)
-        # x = self.block3(x, training=training)
-        # x = self.block4(x, training=training)
-
-        # # Applying Attention
-        # x = self.attention(x)
-
-        # # Final Output Layer
-        # x = self.outlayer(x, training=training)
-
-        x = self.conv(image_inputs)
-
-        return x
+    #     return {"loss": loss}
+        return {"loss": 0.0}
 
 
-class GeneratorBlock(tf.keras.layers.Layer):
+class GeneratorModel(tf.keras.Model):
+    """
+    A generator model that combines latent and vocabulary data for generative tasks.
+    """
 
-    def __init__(self, units, upsample=None):
-        super(GeneratorBlock, self).__init__()
+    def __init__(self, image_shape, latent_dim, vocab_dim, embedding_dim, **kwargs):
+        """
+        Initializes the generator model.
 
-        self.units = units
-        self.upsample = upsample
+        Args:
+            image_shape: list or tuple
+                Shape of the output image.
+            latent_dim: int
+                Dimensionality of the latent space.
+            vocab_dim: int
+                Size of the vocabulary used in embeddings.
+            embedding_dim: int
+                Dimension of the embedding space.
+            **kwargs
+                Additional keyword arguments for `tf.keras.Model`.
+        """
 
-        self.conv1 = tf.keras.layers.SpectralNormalization(
-            tf.keras.layers.Conv2D(filters=units, kernel_size=3, padding='same'))
+        super().__init__(**kwargs)
 
-        self.conv2 = tf.keras.layers.SpectralNormalization(
-            tf.keras.layers.Conv2D(filters=units, kernel_size=3, padding='same'))
+        self.image_shape = image_shape
+        self.latent_dim = latent_dim
+        self.vocab_dim = vocab_dim
+        self.embedding_dim = embedding_dim
 
-        self.conv3 = tf.keras.layers.SpectralNormalization(
-            tf.keras.layers.Conv2D(filters=units, kernel_size=1, padding='same'))
+        self.build_model()
 
-        self.bn1 = ConditionalBatchNormalization(units=units)
-        self.bn2 = ConditionalBatchNormalization(units=units)
+    def summary(self, line_length=None, positions=None, print_fn=None):
+        """
+        Prints a string summary of the network.
 
-        self.activation = tf.keras.layers.ReLU()
+        Args:
+            line_length: int, optional
+                Total length of printed lines.
+            positions: list of float, optional
+                Positions of log elements in each line.
+            print_fn: callable, optional
+                Function used for printing the summary.
+        """
 
-    def call(self, x, y, training=True):
-        h = self.activation(self.bn1(x, y, training=training))
+        self.model.summary(line_length, positions, print_fn)
 
-        if self.upsample is not None:
-            new_height = int(x.shape[1] * self.upsample[0])
-            new_width = int(x.shape[2] * self.upsample[1])
-            h = tf.image.resize(h, [new_height, new_width])
-            x = tf.image.resize(x, [new_height, new_width])
+    def call(self, inputs, training=None, mask=None):
+        """
+        Executes the model on new inputs.
 
-        h = self.conv1(h)
-        h = self.activation(self.bn2(h, y, training=training))
-        h = self.conv2(h)
+        Args:
+            inputs: tensor or collection of tensors
+                The input data to the model.
+            training: bool, optional
+                If True, the model is run in training mode.
+            mask: tensor or collection of tensors, optional
+                An optional mask (or masks) to be applied on the inputs.
 
-        x = self.conv3(x)
+        Returns:
+            tensor or list of tensors
+                The output from the model after processing the inputs.
+        """
 
-        return h + x
+        return self.model(inputs, training, mask)
+
+    def build_model(self):
+        """
+        Builds the generator model with a series of layers including residual blocks, 
+        batch normalization, and convolutional layers.
+
+        The model processes input data through these layers to generate an output image.
+        """
+
+        def residual_block_up(dense, dense_latent, embedding, index, channels, blocks, upsampling=(1, 1)):
+            """
+            Constructs an upsampling residual block within the generator model.
+
+            Args:
+                dense: tensor
+                    Input tensor for the residual block.
+                dense_latent: tensor
+                    Latent feature tensor to be split and concatenated in the block.
+                embedding: tensor
+                    Embedding tensor to be concatenated with latent features.
+                index: int
+                    Index for splitting the latent features.
+                channels: int
+                    Number of output channels for the convolutions.
+                blocks: int
+                    Total number of blocks to determine the split size.
+                upsampling: tuple, optional
+                    Upsampling factor for spatial dimensions.
+
+            Returns:
+                tensor
+                    Output tensor of the residual block.
+            """
+
+            chunk = tf.keras.layers.Lambda(
+                lambda x: tf.split(x, num_or_size_splits=dense_latent.shape[-1]//blocks, axis=-1)[index],
+                name=f'split_{index+1}')(dense_latent)
+
+            dense_concat = tf.keras.layers.Concatenate(axis=-1)([chunk, embedding])
+
+            block_a = tf.keras.layers.UpSampling2D(size=upsampling, interpolation='nearest')(dense)
+            block_a = SpectralNormalization(
+                tf.keras.layers.Conv2D(channels, 1))(block_a)
+
+            block_b = ConditionalBatchNormalization()([dense, dense_concat])
+            block_b = tf.keras.layers.ReLU()(block_b)
+            block_b = tf.keras.layers.UpSampling2D(size=upsampling, interpolation='nearest')(block_b)
+            block_b = SpectralNormalization(
+                tf.keras.layers.Conv2D(channels, 3, padding='same'))(block_b)
+
+            block_b = ConditionalBatchNormalization()([block_b, dense_concat])
+            block_b = tf.keras.layers.ReLU()(block_b)
+            block_b = SpectralNormalization(
+                tf.keras.layers.Conv2D(channels, 3, padding='same'))(block_b)
+
+            block = tf.keras.layers.Add()([block_a, block_b])
+
+            return block
+
+        channels = 128
+        blocks = [128, 64, 64, 32]
+
+        latent_inputs = tf.keras.layers.Input(shape=(self.latent_dim,))
+        vocab_inputs = tf.keras.layers.Input(shape=(self.vocab_dim,))
+
+        vocab_embedding = tf.keras.layers.Embedding(input_dim=self.vocab_dim + 1,
+                                                    output_dim=self.embedding_dim,
+                                                    mask_zero=True)(vocab_inputs)
+
+        latent_expanded = tf.keras.layers.Lambda(
+            lambda x: tf.expand_dims(x, axis=1), name='expand_dims')(latent_inputs)
+
+        latent_tiled = tf.keras.layers.Lambda(
+            lambda x: tf.tile(x[0], [1, tf.shape(x[1])[1], 1]), name='tile')([latent_expanded, vocab_embedding])
+
+        latent_vocab_concat = tf.keras.layers.Concatenate(axis=-1)([latent_tiled, vocab_embedding])
+
+        latent_dense = SpectralNormalization(tf.keras.layers.Dense(units=4*4*channels))(latent_vocab_concat)
+
+        latent_feature_dense = SpectralNormalization(
+            tf.keras.layers.Dense(units=self.latent_dim*len(blocks)))(latent_dense)
+
+        latent_reshaped = tf.keras.layers.Lambda(
+            lambda x: tf.reshape(x, [-1, tf.shape(x)[1]*4, 4, channels]), name='reshape')(latent_dense)
+
+        block = tf.keras.layers.Lambda(
+            lambda x: tf.transpose(x, perm=[0, 3, 2, 1]), name='transpose')(latent_reshaped)
+
+        for i, x in enumerate(blocks):
+            block = residual_block_up(dense=block,
+                                      dense_latent=latent_feature_dense,
+                                      embedding=vocab_embedding,
+                                      index=i,
+                                      channels=x,
+                                      blocks=len(blocks),
+                                      upsampling=(2, 2))
+
+            if i > 0 and blocks[i] == blocks[i-1]:
+                block = SpectralSelfAttention()(block)
+
+        outputs = tf.keras.layers.BatchNormalization()(block)
+        outputs = tf.keras.layers.ReLU()(outputs)
+
+        outputs = DynamicReshape(target_shape=self.image_shape)(outputs)
+
+        outputs = SpectralNormalization(
+            tf.keras.layers.Conv2D(1, 3, padding='same', activation='tanh'))(outputs)
+
+        self.model = tf.keras.Model(inputs=[latent_inputs, vocab_inputs], outputs=outputs)
