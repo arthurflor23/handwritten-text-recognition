72975732dea7ef8bf4a6994b2f1ec55d06d5227f
Arthur Flor <arthurflor23@gmail.com>
2023-11-22 09:17:00 -0300

    Add ExtractPatches layer

--- graphite/layers/processing.py
+++ graphite/layers/processing.py
@@ -9,7 +9,7 @@ class DynamicReshape(tf.keras.layers.Layer):
 
     def __init__(self, target_shape, **kwargs):
         """
-        Initializes the DynamicReshape layer.
+        Initializes DynamicReshape layer.
 
         Args:
             target_shape: list of int
@@ -39,18 +39,18 @@ class DynamicReshape(tf.keras.layers.Layer):
                 The reshaped tensor.
         """
 
-        input_shape_prod = 1
+        shape_prod = 1
         for x in inputs.get_shape()[1:]:
-            input_shape_prod *= x
+            shape_prod *= x
 
-        new_shape = input_shape_prod // self.target_shape_prod
+        new_shape = shape_prod // self.target_shape_prod
         new_shape = [-1] + self.target_shape + [new_shape]
 
         return tf.reshape(inputs, new_shape)
 
     def get_config(self):
         """
-        Returns the config of the wrapper.
+        Returns the config of the layer.
 
         Returns:
             A dictionary containing the configuration of the layer.
@@ -59,6 +59,66 @@ class DynamicReshape(tf.keras.layers.Layer):
         config = {
             "target_shape": self.target_shape,
             "target_shape_prod": self.target_shape_prod,
+        }
+        base_config = super().get_config()
+        return {**base_config, **config}
+
+
+class ExtractPatches(tf.keras.layers.Layer):
+    """
+    A Tensorflow Keras layer to extract patches from input images.
+    """
+
+    def __init__(self, patch_shape):
+        """
+        Initializes Patches layer.
+
+        Args:
+            patch_shape: list or tuple
+                The target patch size to create.
+            **kwargs
+                Additional keyword arguments for the Layer.
+        """
+
+        super().__init__()
+
+        self.patch_shape = list(patch_shape)
+
+    def call(self, inputs):
+        """
+        Splits the input image into patches.
+
+        Args:
+            inputs: tensor
+                The input tensor representing images.
+
+        Returns:
+            tensor
+                A tensor containing the extracted patches.
+        """
+
+        shape = inputs.get_shape()
+
+        patches = tf.image.extract_patches(
+            images=inputs,
+            sizes=[1, shape[1]//self.patch_shape[0], shape[2]//self.patch_shape[1], 1],
+            strides=[1, shape[1]//self.patch_shape[0], shape[2]//self.patch_shape[1], 1],
+            rates=[1, 1, 1, 1],
+            padding='VALID',
+        )
+
+        return patches
+
+    def get_config(self):
+        """
+        Returns the config of the layer.
+
+        Returns:
+            A dictionary containing the configuration of the layer.
+        """
+
+        config = {
+            "patch_shape": self.patch_shape,
         }
         base_config = super().get_config()
         return {**base_config, **config}
