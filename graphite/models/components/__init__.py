8be12ff9ac7aec5f35c4c3642e8add8f1749fb33
Arthur Flor <arthurflor23@gmail.com>
2023-11-26 11:53:00 -0300

    Add loss references

--- graphite/models/components/__init__.py
+++ graphite/models/components/__init__.py
@@ -2,3 +2,5 @@ from models.components.attention import SpectralSelfAttention
 from models.components.normalization import ConditionalBatchNormalization
 from models.components.normalization import SpectralNormalization
 from models.components.processing import ExtractPatches
+from models.components.loss import CTCLoss
+from models.components.loss import CXLoss
