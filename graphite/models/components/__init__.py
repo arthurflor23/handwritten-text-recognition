1e8f8482f37cce2b391d4f336ed9ad760307bbb1
Arthur Flor <arthurflor23@gmail.com>
2023-11-28 20:58:15 -0300

    Update module reference

--- graphite/models/components/__init__.py
+++ graphite/models/components/__init__.py
@@ -3,4 +3,4 @@ from models.components.normalization import ConditionalBatchNormalization
 from models.components.normalization import SpectralNormalization
 from models.components.processing import ExtractPatches
 from models.components.loss import CTCLoss
-from models.components.loss import CXLoss
+from models.components.loss import CTXLoss
