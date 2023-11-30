9f7834f451ca80131e7f7c508f9aad2f5d54f894
Arthur Flor <arthurflor23@gmail.com>
2023-11-30 20:27:33 -0300

    Add optimizer component

---
+++ graphite/models/components/optimizer.py
@@ -0,0 +1,218 @@
+import tensorflow as tf
+
+
+class NormalizedOptimizer(tf.keras.optimizers.Optimizer):
+    """
+    Custom optimizer wrapper for applying gradient normalization.
+
+    This optimizer wraps around another tf.keras optimizer and normalizes
+    the gradients according to a specified norm.
+    """
+
+    def __init__(self, optimizer, normalization='l2', epsilon=1e-7, name='normalized_optimizer', **kwargs):
+        """
+        Initializes the NormalizedOptimizer.
+
+        Args:
+            optimizer: tf.keras.optimizers.Optimizer
+                The optimizer to wrap.
+            normalization: str, optional
+                The type of normalization.
+            epsilon: float, optional
+                Small float for numerical stability.
+            name: str, optional
+                Optional name for the operations created.
+            **kwargs:
+                Additional keyword arguments.
+        """
+
+        super().__init__(name, **kwargs)
+
+        self.optimizer = optimizer
+        self.normalization = normalization
+        self.epsilon = epsilon
+
+        fn = {
+            'avg_l1_l2': self._average_l1_l2_norm,
+            'avg_l1': self._average_l1_norm,
+            'avg_l2': self._average_l2_norm,
+            'l1_l2': self._l1_l2_norm,
+            'l1': self._l1_norm,
+            'l2': self._l2_norm,
+            'max': self._max_norm,
+            'min_max': self._min_max_norm,
+            'std': self._std_norm,
+        }
+
+        self.fn = fn[normalization]
+
+    def apply_gradients(self, grads_and_vars, name=None, skip_gradients_aggregation=False):
+        """
+        Apply gradients to variables.
+
+        Args:
+            grads_and_vars:
+                List of (gradient, variable) pairs.
+            name: str, optional
+                Optional name for the operations created.
+            skip_gradients_aggregation: bool, optional
+                If True, skip the gradient normalization.
+
+        Returns:
+            An `Operation` that applies the specified gradients.
+        """
+
+        if not skip_gradients_aggregation:
+            grads_and_vars = [(grad / (self.fn(grad) + self.epsilon), var) for grad, var in grads_and_vars]
+
+        return self.optimizer.apply_gradients(grads_and_vars, name=name)
+
+    def _average_l1_l2_norm(self, grad):
+        """
+        Computes the average of the L-1 and L-2 norms of the gradient.
+
+        # Arguments:
+            grad: gradient for a variable
+
+        # Returns:
+            The norm of the gradient
+        """
+
+        return (self._average_l1_norm(grad) + self._average_l2_norm(grad)) / 2.
+
+    def _average_l1_norm(self, grad):
+        """
+        Computes the L2 norm of the gradient.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return tf.reduce_mean(tf.abs(grad))
+
+    def _average_l2_norm(self, grad):
+        """
+        Computes the average of the L2 norm of the gradient.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return tf.sqrt(tf.reduce_mean(tf.square(grad)))
+
+    def _l1_l2_norm(self, grad):
+        """
+        Computes the average of the L1 and L2 norms of the gradient.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return (self._l1_norm(grad) + self._l2_norm(grad)) / 2.
+
+    def _l1_norm(self, grad):
+        """
+        Computes the L1 norm of the gradient.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return tf.reduce_sum(tf.abs(grad))
+
+    def _l2_norm(self, grad):
+        """
+        Computes the L2 norm of the gradient.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return tf.sqrt(tf.reduce_sum(tf.square(grad)))
+
+    def _max_norm(self, grad):
+        """
+        Computes the L-infinity norm of the gradient.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return tf.math.maximum(tf.math.abs(grad))
+
+    def _min_max_norm(self, grad):
+        """
+        Computes the average of the Max and Min of the absolute values of the gradients.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return (tf.math.maximum(tf.math.abs(grad)) + tf.math.minimum(tf.math.abs(grad))) / 2.
+
+    def _std_norm(self, grad):
+        """
+        Computes the standard deviation of the gradient.
+
+        Args:
+            grad: A tensor representing a gradient.
+
+        Returns:
+            The norm of the gradient
+        """
+
+        return tf.math.reduce_std(grad)
+
+    def get_config(self):
+        """
+        Returns the config of the wrapper.
+
+        Returns:
+            A dictionary containing the configuration of the wrapper.
+        """
+
+        config = {
+            "optimizer": tf.keras.optimizers.serialize(self.optimizer),
+            "normalization": self.normalization,
+            "epsilon": self.epsilon,
+        }
+        base_config = super().get_config()
+        return {**base_config, **config}
+
+    @classmethod
+    def from_config(cls, config):
+        """
+        Creates an optimizer from its configuration.
+
+        Args:
+            config: A Python dictionary containing the optimizer configuration.
+
+        Returns:
+            A `NormalizedOptimizer` instance.
+        """
+
+        optimizer = tf.keras.optimizers.deserialize(config.pop('optimizer'))
+        normalization = config.pop('normalization', 'l2')
+        epsilon = config.pop('epsilon', 1e-7)
+        return cls(optimizer, normalization, epsilon, **config)
