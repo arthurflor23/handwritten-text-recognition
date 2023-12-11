da603dd0df9aeab6211948a3be853d2472fc178b
Arthur Flor <arthurflor23@gmail.com>
2023-12-11 15:05:34 -0300

    Add processing batches methods Fix issues

--- graphite/data/utils.py
+++ graphite/data/utils.py
@@ -1,6 +1,149 @@
 import re
 import cv2
 import html
+import numpy as np
+
+
+def format_text(text):
+    """
+    Clean and format the input text.
+
+    Parameters
+    ----------
+    text : str
+        The input text to be cleaned.
+
+    Returns
+    -------
+    str or list
+        The formatted text.
+    """
+
+    substitutions = {
+        r'[ ]': ' ',
+        r'[＿]': '_',
+        r'[，]': ',',
+        r'[；]': ';',
+        r'[：]': ':',
+        r'[！﹗]': '!',
+        r'[？﹖]': '?',
+        r'[．。]': '.',
+        r'[＂“”″‶]': '"',
+        r'[（]': '(',
+        r'[）]': ')',
+        r'[［]': '[',
+        r'[］]': ']',
+        r'[｛]': '}',
+        r'[｝]': '{',
+        r'[＠]': '@',
+        r'[＊]': '*',
+        r'[／]': '/',
+        r'[＼]': '\\\\',
+        r'[＆]': '&',
+        r'[＃]': '#',
+        r'[％]': '%',
+        r'[＾]': '^',
+        r'[˗֊‐‑‒–—－−﹣]': '-',
+        r'[＋]': '+',
+        r'[＜]': '<',
+        r'[＝]': '=',
+        r'[＞]': '>',
+        r'[｜]': '|',
+        r'[～]': '~',
+        r'[⋯]': '...',
+        r'[＄]': '$',
+        r"[＇ʼ´‘’‛′‵`᾽᾿՚׳❛❜｀`]": '\'',
+    }
+
+    regexes = {re.compile(pattern): replacement for pattern, replacement in substitutions.items()}
+
+    for pattern, replacement in regexes.items():
+        text = pattern.sub(replacement, text)
+
+    text = html.unescape(text)
+    text = re.sub(r'[ \t]+', ' ', text).strip()
+    text = re.sub(r'\n\s+|\s+\n', '\n', text).strip()
+
+    return text
+
+
+def prepare_text_batch(text_batch, target_shape=None, pad_value=0, dtype=np.int32):
+    """
+    Pads a batch of text data to a uniform shape.
+
+    Parameters
+    ----------
+    text_batch : list
+        List of texts.
+    target_shape : tuple, optional
+        Target shape for padding.
+    pad_value : int, optional
+        Value used for padding.
+    dtype : data-type, optional
+        Data type of the output array.
+
+    Returns
+    -------
+    numpy.ndarray
+        Padded text data.
+    """
+
+    if target_shape:
+        max_paragraphs, max_lines, max_chars = target_shape[:3]
+    else:
+        max_paragraphs = max(len(text) for text in text_batch)
+        max_lines = max(len(paragraph) for text in text_batch for paragraph in text)
+        max_chars = max(len(line) for text in text_batch for paragraph in text for line in paragraph)
+
+    padded = np.full((len(text_batch), max_paragraphs, max_lines, max_chars), pad_value, dtype=dtype)
+
+    for i, text in enumerate(text_batch):
+        paragraphs, lines, chars = np.asarray(text).shape
+        padded[i, :paragraphs, :lines, :chars] = text
+
+    text_batch = np.expand_dims(padded, axis=-1)
+
+    return text_batch
+
+
+def prepare_image_batch(image_batch, target_shape=None, pad_value=255, dtype=np.uint8):
+    """
+    Pads and processes a batch of image data to a uniform shape.
+
+    Parameters
+    ----------
+    image_batch : list
+        List of image arrays.
+    target_shape : tuple, optional
+        Target shape for padding.
+    pad_value : int, optional
+        Value used for padding.
+    dtype : data-type, optional
+        Data type of the output array.
+
+    Returns
+    -------
+    numpy.ndarray
+        Padded and processed image data.
+    """
+
+    if target_shape:
+        max_height, max_width = target_shape[:2]
+    else:
+        max_height = max(len(height) for height in image_batch)
+        max_width = max(len(width) for image in image_batch for width in image)
+
+    padded = np.full((len(image_batch), max_height, max_width), pad_value, dtype=dtype)
+
+    for i, image in enumerate(image_batch):
+        height, width = np.asarray(image).shape
+        padded[i, :height, :width] = image
+
+    image_batch = np.expand_dims(padded, axis=-1)
+    image_batch = image_batch.transpose((0, 2, 1, 3))
+    image_batch = (image_batch.astype(np.float32) / 127.5) - 1
+
+    return image_batch
 
 
 def read_image(image_path, bbox=None, image_shape=None):
@@ -104,66 +247,3 @@ def resize_image(image, target_shape):
         image = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_AREA)
 
     return image
-
-
-def format_text(text):
-    """
-    Clean and format the input text.
-
-    Parameters
-    ----------
-    text : str
-        The input text to be cleaned.
-
-    Returns
-    -------
-    str or list
-        The formatted text.
-    """
-
-    substitutions = {
-        r'[ ]': ' ',
-        r'[＿]': '_',
-        r'[，]': ',',
-        r'[；]': ';',
-        r'[：]': ':',
-        r'[！﹗]': '!',
-        r'[？﹖]': '?',
-        r'[．。]': '.',
-        r'[＂“”″‶]': '"',
-        r'[（]': '(',
-        r'[）]': ')',
-        r'[［]': '[',
-        r'[］]': ']',
-        r'[｛]': '}',
-        r'[｝]': '{',
-        r'[＠]': '@',
-        r'[＊]': '*',
-        r'[／]': '/',
-        r'[＼]': '\\\\',
-        r'[＆]': '&',
-        r'[＃]': '#',
-        r'[％]': '%',
-        r'[＾]': '^',
-        r'[˗֊‐‑‒–—－−﹣]': '-',
-        r'[＋]': '+',
-        r'[＜]': '<',
-        r'[＝]': '=',
-        r'[＞]': '>',
-        r'[｜]': '|',
-        r'[～]': '~',
-        r'[⋯]': '...',
-        r'[＄]': '$',
-        r"[＇ʼ´‘’‛′‵`᾽᾿՚׳❛❜｀`]": '\'',
-    }
-
-    regexes = {re.compile(pattern): replacement for pattern, replacement in substitutions.items()}
-
-    for pattern, replacement in regexes.items():
-        text = pattern.sub(replacement, text)
-
-    text = html.unescape(text)
-    text = re.sub(r'[ \t]+', ' ', text).strip()
-    text = re.sub(r'\n\s+|\s+\n', '\n', text).strip()
-
-    return text
