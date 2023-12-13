1aaa32d463081c839b6dbc809fa7146b8e5ddf35
Arthur Flor <arthurflor23@gmail.com>
2023-12-13 09:27:46 -0300

    Add OctConv2D layer Update GatedConv2D layer Fix issues and patterns

--- graphite/models/components/convolution.py
+++ graphite/models/components/convolution.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-class GatedConv2D(tf.keras.layers.Conv2D):
+class GatedConv2D(tf.keras.layers.Layer):
     """
     Implements gated convolutional layer for TensorFlow.
     Combines linear and sigmoid activations for convolutional gating.
@@ -15,41 +15,60 @@ class GatedConv2D(tf.keras.layers.Conv2D):
         https://arxiv.org/abs/1612.08083
     """
 
-    def __init__(self, filters, kernel_size, use_partial_gating=False, **kwargs):
+    def __init__(self,
+                 filters,
+                 kernel_size=(3, 3),
+                 strides=(1, 1),
+                 padding='same',
+                 kernel_initializer='glorot_uniform',
+                 kernel_regularizer=None,
+                 kernel_constraint=None,
+                 fullgate=True,
+                 **kwargs):
         """
         Initializes the gated convolutional layer.
 
         Parameters
         ----------
         filters : int
-            Number of filters for the convolution.
-        kernel_size : int or tuple/list
-            The size of the convolution window.
-        use_partial_gating : bool, optional
-            Whether to use partial gating.
+            Number of output filters.
+        kernel_size : tuple of 2 ints, optional
+            Convolution window size.
+        strides : tuple of 2 ints, optional
+            Convolution strides.
+        padding : str, optional
+            Padding type.
+        kernel_initializer : initializer, optional
+            Kernel weights initializer.
+        kernel_regularizer : regularizer, optional
+            Kernel weights regularizer.
+        kernel_constraint : constraint, optional
+            Kernel weights constraint.
+        fullgate : bool, optional
+            Whether to use full gating.
         **kwargs : dict
             Conv2D keyword arguments.
         """
 
-        super().__init__(filters, kernel_size, **kwargs)
+        super().__init__(**kwargs)
 
         self.filters = filters
         self.kernel_size = kernel_size
-        self.use_partial_gating = use_partial_gating
-
-        if self.use_partial_gating:
-            self.conv = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, **kwargs)
-        else:
-            self.conv = tf.keras.layers.Conv2D(filters=filters * 2, kernel_size=kernel_size, **kwargs)
+        self.strides = strides
+        self.padding = padding
+        self.kernel_initializer = kernel_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.kernel_constraint = kernel_constraint
+        self.fullgate = fullgate
 
     def get_config(self):
         """
-        Returns the config of the optimizer.
+        Returns the config of the layer.
 
         Returns
         -------
         dict
-            A dictionary containing the configuration of the optimizer.
+            A dictionary containing the configuration of the layer.
         """
 
         config = super().get_config()
@@ -57,10 +76,35 @@ class GatedConv2D(tf.keras.layers.Conv2D):
         config.update({
             'filters': self.filters,
             'kernel_size': self.kernel_size,
-            'use_partial_gating': self.use_partial_gating,
+            'strides': self.strides,
+            'padding': self.padding,
+            'kernel_initializer': self.kernel_initializer,
+            'kernel_regularizer': self.kernel_regularizer,
+            'kernel_constraint': self.kernel_constraint,
+            'fullgate': self.fullgate,
         })
 
         return config
+
+    def build(self, input_shape):
+        """
+        Initializes layer weights.
+
+        Parameters
+        ----------
+        input_shape : tuple
+            Shape of the input to the layer.
+        """
+
+        super().build(input_shape)
+
+        self.conv = tf.keras.layers.Conv2D(filters=self.filters * (2 if self.fullgate else 1),
+                                           kernel_size=self.kernel_size,
+                                           strides=self.strides,
+                                           padding=self.padding,
+                                           kernel_initializer=self.kernel_initializer,
+                                           kernel_regularizer=self.kernel_regularizer,
+                                           kernel_constraint=self.kernel_constraint)
 
     def call(self, inputs):
         """
@@ -79,13 +123,206 @@ class GatedConv2D(tf.keras.layers.Conv2D):
 
         conv = self.conv(inputs)
 
-        if self.use_partial_gating:
-            conv = tf.keras.layers.Activation('sigmoid')(conv)
-            outputs = inputs * conv
-        else:
+        if self.fullgate:
             linear, sigmoid = tf.split(conv, 2, axis=-1)
             linear = tf.keras.layers.Activation('linear')(linear)
             sigmoid = tf.keras.layers.Activation('sigmoid')(sigmoid)
             outputs = linear * sigmoid
+        else:
+            conv = tf.keras.layers.Activation('sigmoid')(conv)
+            outputs = inputs * conv
 
         return outputs
+
+
+class OctConv2D(tf.keras.layers.Layer):
+    """
+    Implements octave convolutional layer for TensorFlow.
+    This layer processes input feature maps by splitting them into high and low frequency components
+
+    References
+    ----------
+    Drop an Octave: Reducing Spatial Redundancy in Convolutional Neural Networks with Octave Convolution
+        https://arxiv.org/abs/1904.05049
+    """
+
+    def __init__(self,
+                 alpha,
+                 filters,
+                 kernel_size=(3, 3),
+                 strides=(1, 1),
+                 padding='same',
+                 kernel_initializer='glorot_uniform',
+                 kernel_regularizer=None,
+                 kernel_constraint=None,
+                 **kwargs):
+        """
+        Initialize the octave convolutional layer.
+
+        Parameters
+        ----------
+        alpha : float
+            Fraction of filters for low frequency.
+        filters : int
+            Number of output filters.
+        kernel_size : tuple of 2 ints, optional
+            Convolution window size.
+        strides : tuple of 2 ints, optional
+            Convolution strides.
+        padding : str, optional
+            Padding type.
+        kernel_initializer : initializer, optional
+            Kernel weights initializer.
+        kernel_regularizer : regularizer, optional
+            Kernel weights regularizer.
+        kernel_constraint : constraint, optional
+            Kernel weights constraint.
+        """
+
+        super().__init__(**kwargs)
+
+        self.alpha = alpha
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.kernel_initializer = kernel_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.kernel_constraint = kernel_constraint
+
+        self.low_channels = int(self.filters * self.alpha)
+        self.high_channels = self.filters - self.low_channels
+
+    def get_config(self):
+        """
+        Returns the config of the layer.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the configuration of the layer.
+        """
+
+        config = super().get_config()
+
+        config.update({
+            'alpha': self.alpha,
+            'filters': self.filters,
+            'kernel_size': self.kernel_size,
+            'strides': self.strides,
+            'padding': self.padding,
+            'kernel_initializer': self.kernel_initializer,
+            'kernel_regularizer': self.kernel_regularizer,
+            'kernel_constraint': self.kernel_constraint,
+        })
+
+        return config
+
+    def build(self, input_shape):
+        """
+        Initializes layer weights.
+
+        Parameters
+        ----------
+        input_shape : tuple
+            Shape of the input to the layer.
+        """
+
+        super().build(input_shape)
+
+        high_in = int(input_shape[0][3])
+        low_in = int(input_shape[1][3])
+
+        self.high_to_high_kernel = self.add_weight(name='high_to_high_kernel',
+                                                   shape=(*self.kernel_size, high_in, self.high_channels),
+                                                   initializer=self.kernel_initializer,
+                                                   regularizer=self.kernel_regularizer,
+                                                   constraint=self.kernel_constraint)
+
+        self.high_to_low_kernel = self.add_weight(name='high_to_low_kernel',
+                                                  shape=(*self.kernel_size, high_in, self.low_channels),
+                                                  initializer=self.kernel_initializer,
+                                                  regularizer=self.kernel_regularizer,
+                                                  constraint=self.kernel_constraint)
+
+        self.low_to_high_kernel = self.add_weight(name='low_to_high_kernel',
+                                                  shape=(*self.kernel_size, low_in, self.high_channels),
+                                                  initializer=self.kernel_initializer,
+                                                  regularizer=self.kernel_regularizer,
+                                                  constraint=self.kernel_constraint)
+
+        self.low_to_low_kernel = self.add_weight(name='low_to_low_kernel',
+                                                 shape=(*self.kernel_size, low_in, self.low_channels),
+                                                 initializer=self.kernel_initializer,
+                                                 regularizer=self.kernel_regularizer,
+                                                 constraint=self.kernel_constraint)
+
+    def call(self, inputs):
+        """
+        Processes the input tensors through the layer.
+
+        Parameters
+        ----------
+        inputs : list of two tensors
+            High and low frequency components of the input.
+
+        Returns
+        -------
+        list of two tensors
+            Processed high and low frequency outputs.
+        """
+
+        high_input, low_input = inputs
+
+        high_to_high = tf.nn.conv2d(input=high_input,
+                                    filters=self.high_to_high_kernel,
+                                    strides=[1, *self.strides, 1],
+                                    padding=self.padding.upper())
+
+        high_to_low = tf.nn.avg_pool2d(input=high_input,
+                                       ksize=[1, 2, 2, 1],
+                                       strides=[1, 2, 2, 1],
+                                       padding='SAME')
+
+        high_to_low = tf.nn.conv2d(input=high_to_low,
+                                   filters=self.high_to_low_kernel,
+                                   strides=[1, *self.strides, 1],
+                                   padding=self.padding.upper())
+
+        low_to_high = tf.nn.conv2d(input=low_input,
+                                   filters=self.low_to_high_kernel,
+                                   strides=[1, *self.strides, 1],
+                                   padding=self.padding.upper())
+
+        low_to_high = tf.tile(low_to_high, [1, 2, 2, 1])
+
+        low_to_low = tf.nn.conv2d(input=low_input,
+                                  filters=self.low_to_low_kernel,
+                                  strides=[1, *self.strides, 1],
+                                  padding=self.padding.upper())
+
+        high_add = high_to_high + low_to_high
+        low_add = low_to_low + high_to_low
+
+        return [high_add, low_add]
+
+    def compute_output_shape(self, input_shapes):
+        """
+        Compute the output shape of the layer.
+
+        Parameters
+        ----------
+        input_shape : tuple or list
+            The shape of the input to the layer.
+
+        Returns
+        -------
+        tf.TensorShape
+            The computed shape of the output from the layer.
+        """
+
+        high_in_shape, low_in_shape = input_shapes
+        high_out_shape = (*high_in_shape[:3], self.high_channels)
+        low_out_shape = (*low_in_shape[:3], self.low_channels)
+
+        return [high_out_shape, low_out_shape]
