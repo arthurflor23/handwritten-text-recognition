752a1f335059fa4dbfddc87d0dae9a11dbb3398c
Arthur Flor <arthurflor23@gmail.com>
2023-12-16 16:07:07 -0300

    Update fit method - Add callbacks

--- graphite/models/graphite.py
+++ graphite/models/graphite.py
@@ -6,6 +6,7 @@ import datetime
 import importlib
 import tensorflow as tf
 
+from models.components.callback import GANMonitor
 from models.components.loss import CTCLoss
 from models.components.metric import EditDistance
 from models.components.optimizer import NormalizedOptimizer
@@ -21,8 +22,7 @@ class Graphite():
                  recognition=None,
                  spelling=None,
                  image_shape=None,
-                 lexical_shape=None,
-                 writers_shape=None,
+                 tokenizer=None,
                  synthesis_ratio=1.0,
                  experiment_name='Default'):
         """
@@ -33,8 +33,7 @@ class Graphite():
         self.recognition = recognition
         self.spelling = spelling
         self.image_shape = image_shape
-        self.lexical_shape = lexical_shape
-        self.writers_shape = writers_shape
+        self.tokenizer = tokenizer
         self.synthesis_ratio = synthesis_ratio
         self.experiment_name = experiment_name
 
@@ -45,33 +44,38 @@ class Graphite():
         if workflow is not None:
             self.experiment = mlflow.set_experiment(experiment_name)
 
-            synthesis_model = None
-            recognition_model = None
+            SynthesizerModel = None
+            SynthesizerRecognizerModel = None
 
             if 'synthesis' in self.workflow:
-                SynthesisModel = self._import_model(module=self.synthesis_label, class_name='SynthesisModel')
-
-                synthesis_model = SynthesisModel(image_shape=self.image_shape,
-                                                 lexical_shape=self.lexical_shape,
-                                                 writers_shape=self.writers_shape)
+                SynthesizerModel = self._import_model(module=self.synthesis_label,
+                                                      class_name='SynthesizerModel')
 
             if 'recognition' in self.workflow:
-                RecognitionModel = self._import_model(module=self.recognition_label, class_name='RecognitionModel')
+                SynthesizerRecognizerModel = self._import_model(module=self.recognition_label,
+                                                                class_name='SynthesizerRecognizerModel')
 
-                recognition_model = RecognitionModel(image_shape=self.image_shape,
-                                                     lexical_shape=self.lexical_shape)
+            if SynthesizerModel and not SynthesizerRecognizerModel:
+                self.model = SynthesizerModel(image_shape=self.image_shape,
+                                              lexical_shape=self.tokenizer.lexical_shape,
+                                              writers_shape=self.tokenizer.writers_shape)
+            elif SynthesizerRecognizerModel:
+                synthesizer_params = {}
 
-            if synthesis_model and recognition_model:
-                self.model = CarbonModel(generator=synthesis_model.generator,
-                                         style_encoder=synthesis_model.style_encoder,
-                                         style_backbone=synthesis_model.style_backbone,
-                                         recognition=recognition_model,
-                                         synthesis_ratio=self.synthesis_ratio)
-            elif synthesis_model:
-                self.model = synthesis_model
+                if SynthesizerModel:
+                    synthesizer = SynthesizerModel(image_shape=self.image_shape,
+                                                   lexical_shape=self.tokenizer.lexical_shape,
+                                                   writers_shape=self.tokenizer.writers_shape)
+                    synthesizer_params = {
+                        'style_backbone': synthesizer.style_backbone,
+                        'style_encoder': synthesizer.style_encoder,
+                        'generator': synthesizer.generator,
+                        'synthesis_ratio': self.synthesis_ratio,
+                    }
 
-            elif recognition_model:
-                self.model = recognition_model
+                self.model = SynthesizerRecognizerModel(image_shape=self.image_shape,
+                                                        lexical_shape=self.tokenizer.lexical_shape,
+                                                        **synthesizer_params)
 
         # with mlflow.start_run(run_id=None, run_name=str(datetime.datetime.now())) as _:
         #     mlflow.set_tags({'graphite.label': 'synthesis:gan,recognition:bluche'})
@@ -95,14 +99,19 @@ class Graphite():
 
     def _import_model(self, module, class_name):
         """
-        Imports the model with the given module and name.
+        Dynamically imports and returns a class from a specified module.
 
         Parameters
         ----------
         module : str
-            The name of the module containing the model.
+            Module name containing the class.
         class_name : str
-            The name of the class to import from the model.
+            Class name to be imported.
+
+        Returns
+        -------
+        class
+            The dynamically imported class.
         """
 
         module_name = importlib.util.resolve_name(f".{module}", __package__)
@@ -114,6 +123,150 @@ class Graphite():
 
         model = getattr(module, class_name)
         return model
+
+    def compile(self, learning_rate=0.001):
+        """
+        Configure the submodels for training.
+
+        Parameters
+        ----------
+        learning_rate : float, optional
+            The learning rate for the optimizer.
+        """
+
+        self.model.compile(learning_rate=learning_rate)
+
+    def fit(self,
+            training_data,
+            training_steps=None,
+            validation_data=None,
+            validation_steps=None,
+            monitor_samples_data=None,
+            monitor_samples_steps=None,
+            plateau_factor=0.1,
+            plateau_cooldown=0,
+            plateau_patience=20,
+            patience=30,
+            epochs=None):
+        """
+        Trains the model.
+
+        Parameters
+        ----------
+        training_data : generator
+            Generator yielding training data batches.
+        training_steps : int, optional
+            Number of steps per training epoch.
+        validation_data : generator, optional
+            Generator yielding validation data batches.
+        validation_steps : int, optional
+            Number of steps per validation run.
+        monitor_samples_data : generator, optional
+            Generator yielding samples data batches.
+        monitor_samples_steps : int, optional
+            Number of steps per sample run.
+        plateau_factor : float, optional
+            Factor for reducing the learning rate.
+        plateau_cooldown : int, optional
+            Epochs to wait after a learning rate reduction.
+        plateau_patience : int, optional
+            Epochs without improvement before reducing the learning rate.
+        patience : int, optional
+            Epochs without improvement before stopping training.
+        epochs : int, optional
+            Number of training epochs.
+
+        Returns
+        -------
+        history object
+            Training and validation progress details.
+        """
+
+        start_time = datetime.datetime.now()
+
+        with mlflow.start_run(run_name=str(start_time)) as run:
+            self.run = run
+
+            artifact_uri = os.path.join(run.info.artifact_uri, 'artifacts')
+            artifact_path = artifact_uri.replace('file://', '')
+
+            logs_path = os.path.join(artifact_path, 'logs')
+            os.makedirs(logs_path, exist_ok=True)
+
+            tensorboard_path = os.path.join(artifact_path, 'tensorboard')
+            os.makedirs(tensorboard_path, exist_ok=True)
+
+            callbacks = [
+                # tf.keras.callbacks.CSVLogger(
+                #     filename=os.path.join(logs_path, 'epochs.log'),
+                #     separator=',',
+                #     append=True,
+                # ),
+                # tf.keras.callbacks.ModelCheckpoint(
+                #     filepath=os.path.join(artifact_path, 'model.keras'),
+                #     mode='min',
+                #     monitor=self.model.monitor,
+                #     save_freq='epoch',
+                #     save_best_only=True,
+                #     save_weights_only=True,
+                #     verbose=1,
+                # ),
+                # tf.keras.callbacks.TensorBoard(
+                #     log_dir=tensorboard_path,
+                #     histogram_freq=0,
+                #     write_graph=True,
+                #     write_images=False,
+                #     write_steps_per_second=False,
+                #     update_freq='epoch',
+                #     profile_batch=0,
+                #     embeddings_freq=0,
+                #     embeddings_metadata=None,
+                # ),
+                # tf.keras.callbacks.EarlyStopping(
+                #     mode='min',
+                #     monitor=self.model.monitor,
+                #     min_delta=1e-8,
+                #     patience=patience,
+                #     start_from_epoch=0,
+                #     restore_best_weights=True,
+                #     verbose=1,
+                # ),
+                # tf.keras.callbacks.ReduceLROnPlateau(
+                #     mode='min',
+                #     monitor=self.model.monitor,
+                #     min_lr=1e-4,
+                #     min_delta=1e-8,
+                #     factor=plateau_factor,
+                #     cooldown=plateau_cooldown,
+                #     patience=plateau_patience,
+                #     verbose=1,
+                # ),
+            ]
+
+            if 'None' not in self.synthesis_label and \
+                    monitor_samples_data is not None and \
+                    monitor_samples_steps is not None:
+
+                samples_path = os.path.join(artifact_path, 'samples')
+                os.makedirs(samples_path, exist_ok=True)
+
+                callbacks.extend([
+                    GANMonitor(filepath=samples_path,
+                               sample_data=monitor_samples_data,
+                               sample_steps=monitor_samples_steps,
+                               latent_dim=self.model.generator.latent_dim,
+                               monitor=self.model.monitor),
+                ])
+
+            history = self.model.fit(x=training_data,
+                                     steps_per_epoch=training_steps,
+                                     validation_data=validation_data,
+                                     validation_steps=validation_steps,
+                                     callbacks=callbacks,
+                                     epochs=epochs or int(1e+6),
+                                     verbose=1)
+
+        return history
 
     @staticmethod
     def get_tokenizer(synthesis=None,
@@ -140,10 +293,10 @@ class Graphite():
         Returns
         -------
         tuple
-            (tokenizer, artifacts_uri) or (None, None) if not found.
+            (tokenizer, artifacts_path) or (None, None) if not found.
         """
 
-        def get_artifacts_uri(label, run_index):
+        def get_artifacts_path(label, run_index):
             if run_index is None:
                 return None
 
@@ -158,23 +311,24 @@ class Graphite():
             if not df.empty:
                 run_context = mlflow.get_run(df.iloc[run_index]['run_id'])
                 artifacts_uri = os.path.join(run_context.info.artifact_uri, 'artifacts')
-                return artifacts_uri if os.path.isdir(artifacts_uri) else None
+                artifacts_path = artifacts_uri.replace('file://', '')
+                return artifacts_path if os.path.isdir(artifacts_path) else None
             return None
 
-        synthesis_uri = get_artifacts_uri(label=f"synthesis.{synthesis}", run_index=synthesis_index)
-        recognition_uri = get_artifacts_uri(label=f"recognition.{recognition}", run_index=recognition_index)
+        synthesis_uri = get_artifacts_path(label=f"synthesis.{synthesis}", run_index=synthesis_index)
+        recognition_uri = get_artifacts_path(label=f"recognition.{recognition}", run_index=recognition_index)
 
-        artifacts_uri = (synthesis_uri or recognition_uri)
+        artifacts_path = (synthesis_uri or recognition_uri)
         tokenizer = None
 
-        if artifacts_uri and os.path.isdir(artifacts_uri):
-            tokenizer_uri = os.path.join(artifacts_uri, 'tokenizer.pkl')
+        if artifacts_path and os.path.isdir(artifacts_path):
+            tokenizer_uri = os.path.join(artifacts_path, 'tokenizer.pkl')
 
             if os.path.isfile(tokenizer_uri):
                 with open(tokenizer_uri, 'rb') as f:
                     tokenizer = pickle.load(f)
 
-        return tokenizer, artifacts_uri
+        return tokenizer, artifacts_path
 
 
 class CarbonModel(tf.keras.Model):
