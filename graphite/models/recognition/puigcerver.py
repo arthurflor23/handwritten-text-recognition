9830661f467be50629ee03cf3498a8579131ef5c
Arthur Flor <arthurflor23@gmail.com>
2023-12-16 14:36:54 -0300

    Set new recognizer base model - Reduce code - Improve code

--- graphite/models/recognition/puigcerver.py
+++ graphite/models/recognition/puigcerver.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
 
-from models.components.loss import CTCLoss
-from models.components.metric import EditDistance
-from models.components.optimizer import NormalizedOptimizer
+from models.carbon import SynthesizerRecognizerBaseModel
 
 
-class RecognitionModel(tf.keras.Model):
+class SynthesizerRecognizerModel(SynthesizerRecognizerBaseModel):
     """
     TensorFlow model for multilingual handwriting recognition using CNNs and BiLSTMs.
     It's based on traditional deep learning methods for offline handwriting recognition (CRNN).
@@ -15,97 +13,6 @@ class RecognitionModel(tf.keras.Model):
     Are multidimensional recurrent layers really necessary for handwritten text recognition?
         https://ieeexplore.ieee.org/document/8269951
     """
-
-    def __init__(self,
-                 image_shape,
-                 lexical_shape,
-                 **kwargs):
-        """
-        Initialize the handwriting recognition model with specified parameters.
-
-        Parameters
-        ----------
-        image_shape : list or tuple
-            Shape of the input image.
-        lexical_shape : list or tuple
-            Shape of the text sequences and vocabulary encoding.
-        **kwargs : dict
-            Additional keyword arguments for `tf.keras.Model`.
-        """
-
-        super().__init__(name='recognition', **kwargs)
-
-        self.image_shape = image_shape
-        self.lexical_shape = lexical_shape
-
-        self.build_model()
-
-        if hasattr(self, 'model'):
-            self.summary = self.model.summary
-            self.call = self.model.call
-
-    def __repr__(self):
-        """
-        Provides a formatted string with useful information.
-
-        Returns
-        -------
-        str
-            Formatted string with useful information.
-        """
-
-        info = "=================================================="
-        info += f"\n{self.__class__.__name__.center(50)}"
-
-        trainable_count = sum([tf.size(x).numpy() for x in self.model.trainable_variables])
-        non_trainable_count = sum([tf.size(x).numpy() for x in self.model.non_trainable_variables])
-        total_count = trainable_count + non_trainable_count
-
-        info += "\n--------------------------------------------------"
-        info += f"\n{'Model':<{25}}: {self.model.name}"
-        info += "\n--------------------------------------------------"
-        info += f"\n{'Total params':<{25}}: {total_count:,}"
-        info += f"\n{'Trainable params':<{25}}: {trainable_count:,}"
-        info += f"\n{'Non-trainable params':<{25}}: {non_trainable_count:,}"
-        info += f"\n{'Size (MB)':<{25}}: {(total_count*4) / (1024**2):,.2f}"
-
-        return info
-
-    def get_config(self):
-        """
-        Retrieves the configuration of the model for serialization.
-
-        Returns
-        -------
-        dict
-            A dictionary containing the configuration of the model.
-        """
-
-        config = super().get_config()
-
-        config.update({
-            'image_shape': self.image_shape,
-            'lexical_shape': self.lexical_shape,
-        })
-
-        return config
-
-    def compile(self, learning_rate=0.001):
-        """
-        Configure the submodels for training.
-
-        This method sets up the optimizers, loss functions, and metrics for the model.
-
-        Parameters
-        ----------
-        learning_rate : float, optional
-            The learning rate for the optimizer.
-        """
-
-        optimizer = NormalizedOptimizer(
-            tf.keras.optimizers.AdamW(learning_rate=learning_rate, weight_decay=0.001))
-
-        super().compile(optimizer=optimizer, loss=CTCLoss(), metrics=[EditDistance()], run_eagerly=False)
 
     def build_model(self):
         """
