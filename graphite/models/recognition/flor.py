f51e6fdacf5bbc94a5e37570f28c1018f14bd504
Arthur Flor <arthurflor23@gmail.com>
2023-12-15 23:36:28 -0300

    Update output shape

--- graphite/models/recognition/flor.py
+++ graphite/models/recognition/flor.py
@@ -193,14 +193,14 @@ class RecognitionModel(tf.keras.Model):
 
         conv = tf.keras.layers.MaxPooling2D(pool_size=(1, 2), strides=(1, 2), padding='valid')(conv)
 
-        bgru = tf.keras.layers.Reshape(target_shape=(conv.get_shape()[1], -1))(conv)
-        bgru = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, dropout=0.5))(bgru)
-        bgru = tf.keras.layers.Dense(units=256)(bgru)
+        bgru = tf.keras.layers.Reshape(target_shape=[conv.get_shape()[1], -1])(conv)
 
         bgru = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, dropout=0.5))(bgru)
         bgru = tf.keras.layers.Dense(units=256)(bgru)
 
-        outputs = tf.keras.layers.Reshape(target_shape=self.lexical_shape[:-1] + (-1,))(bgru)
-        outputs = tf.keras.layers.Dense(units=self.lexical_shape[-1], activation='softmax')(outputs)
+        bgru = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128, return_sequences=True, dropout=0.5))(bgru)
+        bgru = tf.keras.layers.Dense(units=self.lexical_shape[-1], activation='softmax')(bgru)
+
+        outputs = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=1), name='expand_dims')(bgru)
 
         self.model = tf.keras.Model(inputs=inputs, outputs=outputs, name=self.name)
