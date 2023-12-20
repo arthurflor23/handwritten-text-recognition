3e06ecaa81fe6c38cd152effbaa00d5b7d80e257
Arthur Flor <arthurflor23@gmail.com>
2023-12-20 19:25:07 -0300

    Add inference pipeline

--- graphite/pipelines/inference.py
+++ graphite/pipelines/inference.py
@@ -1,14 +1,14 @@
-# import cv2
-# import numpy as np
+import os
+import cv2
+import json
 
-# from data import Dataset
-# from models import Model
-# from spelling import Spelling
+from data import Dataset
+from models import Graphite
 
 
 def inference(args):
     """
-    Performs the inference phase.
+    Executes the inference phase.
 
     Parameters
     ----------
@@ -16,68 +16,97 @@ def inference(args):
         A namespace object containing all the arguments required.
     """
 
-    print(args)
+    tokenizer, context = Graphite().get_tokenizer(synthesis=args.synthesis,
+                                                  synthesis_index=args.synthesis_index,
+                                                  recognition=args.recognition,
+                                                  recognition_index=args.recognition_index,
+                                                  experiment_name=args.experiment_name)
 
-    # def print_section(content):
-    #     print(f"\n{'=' * 72}\n{content}\n{'=' * 72}\n")
+    if tokenizer is None or context is None:
+        print('Tokenizer or context not found to load.')
+        return
 
-    # data = [[image, args.bbox, ['']] for image in args.images]
+    data = {
+        'test': [{
+            'image': args.image,
+            'bbox': args.bbox,
+            'text': args.text,
+            'writer': '1',
+        }],
+    }
 
-    # dataset = Dataset(data=data,
-    #                   level=args.level,
-    #                   binarization=args.binarization,
-    #                   eager_mode=args.eager_mode,
-    #                   seed=args.seed)
+    dataset = Dataset(data=data,
+                      image_shape=args.image_shape,
+                      lazy_mode=args.lazy_mode,
+                      tokenizer=tokenizer,
+                      seed=args.seed)
+    print(dataset)
 
-    # if args.verbose > 0:
-    #     print_section(dataset)
+    graphite = Graphite(workflow=args.workflow,
+                        synthesis=args.synthesis,
+                        recognition=args.recognition,
+                        spelling=args.spelling,
+                        image_shape=args.image_shape,
+                        tokenizer=dataset.tokenizer,
+                        synthesis_ratio=args.synthesis_ratio,
+                        experiment_name=args.experiment_name)
+    print(graphite)
 
-    # model = Model(network=args.network, experiment_name=args.experiment_name)
-    # model.compile(run_index=args.run_index)
+    graphite.compile(learning_rate=args.learning_rate)
+    # graphite.compile(learning_rate=args.learning_rate, context=context)
 
-    # if args.verbose > 1:
-    #     print_section(model)
+    output_path = 'outputs'
+    name = os.path.splitext(os.path.basename(args.image or ''))[0]
 
-    # infer_data, infer_steps = dataset.get_generator(partition=dataset.test,
-    #                                                 batch_size=args.batch_size,
-    #                                                 shuffle=False)
+    os.makedirs(output_path, exist_ok=True)
 
-    # predictions, probabilities = model.predict(test_data=infer_data,
-    #                                            test_steps=infer_steps,
-    #                                            top_paths=args.top_paths,
-    #                                            beam_width=args.beam_width,
-    #                                            ctc_decode=True,
-    #                                            token_decode=True,
-    #                                            verbose=args.verbose)
+    if 'recognition' in args.workflow:
+        prediction_configs = [{
+            'predict': True,
+            'corrections': False,
+        }, {
+            'predict': args.spelling,
+            'corrections': True,
+        }]
 
-    # if args.verbose > 0:
-    #     print_section(model.test_logger)
+        for config in prediction_configs:
+            if not config['predict']:
+                continue
 
-    # if args.spell_checker:
-    #     spelling = Spelling(spell_checker=args.spell_checker,
-    #                         api_key=args.api_key,
-    #                         env_key=args.env_key)
+            infer_gen, infer_steps = dataset.get_generator(data_partition='test',
+                                                           batch_size=args.batch_size)
 
-    #     if args.verbose > 1:
-    #         print_section(spelling)
+            predictions, probabilities = graphite.predict_recognition(x=infer_gen,
+                                                                      steps=infer_steps,
+                                                                      top_paths=args.top_paths,
+                                                                      beam_width=args.beam_width,
+                                                                      ctc_decode=True,
+                                                                      token_decode=True,
+                                                                      corrections=config['corrections'])
 
-    #     predictions = spelling.enhance(predictions, verbose=args.verbose)
+            content = []
+            for x, p in zip(predictions, probabilities):
+                for i in range(len(x)):
+                    content.append({
+                        'top_path': i+1,
+                        'probability': p[i],
+                        'prediction': x[i],
+                    })
 
-    # for i in range(dataset.test['size']):
-    #     print("Path   Probability   Predict")
+            content = json.dumps(content, indent=4, sort_keys=False)
+            print(content)
 
-    #     for j in range(args.top_paths):
-    #         probability = np.mean(probabilities[j][i])
-    #         predict = '\n'.join(predictions[j][i])
+            filepath = os.path.join(output_path, f"{name}.json")
+            with open(filepath, 'w') as f:
+                f.write(content)
 
-    #         print(f"{j+1:>4}   {probability:>11.2%}   {predict}")
+    elif 'synthesis' in args.workflow:
+        infer_gen, infer_steps = dataset.get_generator(data_partition='test',
+                                                       batch_size=args.batch_size)
 
-    #     if args.check:
-    #         print("\nPress Enter to continue or Esc to stop...\n")
+        predictions = graphite.predict_synthesis(x=infer_gen, steps=infer_steps)
 
-    #         cv2.imshow("Image", dataset.test['data'][i][0])
-    #         key = cv2.waitKey(0)
-
-    #         if key == 27:
-    #             cv2.destroyAllWindows()
-    #             return
+        filepath = f"{name}_{'guided_style' if args.image else 'random_style'}".strip('_')
+        for i, image in enumerate(predictions):
+            generated_filepath = os.path.join(output_path, f"{filepath}.png")
+            cv2.imwrite(generated_filepath, image)
