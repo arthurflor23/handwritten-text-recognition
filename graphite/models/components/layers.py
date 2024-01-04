f7d9b1769a43f199f0c3b9e3c672e56226fdb25c
Arthur Flor <arthurflor23@gmail.com>
2024-01-04 15:58:10 -0300

    Add weight normalization layer class

--- graphite/models/components/layers.py
+++ graphite/models/components/layers.py
@@ -1197,3 +1197,208 @@ class TemporalResidualBlock(tf.keras.layers.Layer):
             self.build(input_shape)
 
         return tf.TensorShape([self.res_output_shape, self.res_output_shape])
+
+
+class WeightNormalization(tf.keras.layers.Wrapper):
+    """
+    Performs weight normalization.
+    This wrapper reparameterizes a layer by decoupling the weight's magnitude and direction.
+    This speeds up convergence by improving the conditioning of the optimization problem.
+
+    References
+    ----------
+    Weight Normalization: A Simple Reparameterization to Accelerate Training of Deep Neural Networks
+        https://arxiv.org/abs/1602.07868
+    """
+
+    def __init__(self, layer, data_init=True, **kwargs):
+        """
+        Initializes the weight normalization wrapper.
+
+        Parameters
+        ----------
+        layer : tf.keras.layers.Layer
+            The layer to be wrapped.
+        data_init : bool, optional
+            Whether to use data-dependent initialization.
+        **kwargs : dict
+            Additional keyword arguments for the wrapper.
+
+        Raises
+        ------
+        ValueError
+            If data_init is True and the layer is an RNN, as advised against in the paper.
+        """
+
+        super().__init__(layer, **kwargs)
+
+        self.data_init = data_init
+        self._track_trackable(layer, name='layer')
+        self.is_rnn = isinstance(self.layer, tf.keras.layers.RNN)
+
+        if self.data_init and self.is_rnn:
+            raise ValueError('WeightNormalization: Using `data_init=True` with RNNs '
+                             'is advised against by the paper. Use `data_init=False`.')
+
+    def build(self, input_shape):
+        """
+        Build the WeightNormalization wrapper.
+
+        Parameters
+        ----------
+        input_shape : tuple
+            The expected input shape for the layer.
+        """
+
+        input_shape = tf.TensorShape(input_shape)
+        self.input_spec = tf.keras.layers.InputSpec(shape=[None] + input_shape[1:])
+
+        if not self.layer.built:
+            self.layer.build(input_shape)
+
+        kernel_layer = self.layer.cell if self.is_rnn else self.layer
+
+        if not hasattr(kernel_layer, 'kernel'):
+            raise ValueError('`WeightNormalization` must wrap a layer that '
+                             'contains a `kernel` for weights')
+
+        if self.is_rnn:
+            kernel = kernel_layer.recurrent_kernel
+        else:
+            kernel = kernel_layer.kernel
+
+        self.layer_depth = int(kernel.shape[-1])
+        self.kernel_norm_axes = list(range(kernel.shape.rank - 1))
+
+        self.v = kernel
+
+        self.g = self.add_weight(name='g',
+                                 shape=(self.layer_depth,),
+                                 initializer='ones',
+                                 dtype=kernel.dtype,
+                                 trainable=True)
+
+        self._initialized = self.add_weight(name='initialized',
+                                            shape=None,
+                                            initializer='zeros',
+                                            dtype=tf.dtypes.bool,
+                                            trainable=False)
+
+        if self.data_init:
+            with tf.name_scope('data_dep_init'):
+                layer_config = tf.keras.layers.serialize(self.layer)
+                layer_config['config']['trainable'] = False
+
+                self._naked_clone_layer = tf.keras.layers.deserialize(layer_config)
+                self._naked_clone_layer.build(input_shape)
+                self._naked_clone_layer.set_weights(self.layer.get_weights())
+
+                if not self.is_rnn:
+                    self._naked_clone_layer.activation = None
+
+        self.built = True
+
+    def call(self, inputs):
+        """
+        Call the wrapped layer with inputs.
+
+        Parameters
+        ----------
+        inputs : tensor
+            Input tensor.
+
+        Returns
+        -------
+        tensor
+            The output tensor of the layer.
+        """
+
+        def _do_nothing():
+            return tf.identity(self.g)
+
+        def _update_weights():
+            with tf.control_dependencies(self._initialize_weights(inputs)):
+                return tf.identity(self.g)
+
+        g = tf.cond(self._initialized, _do_nothing, _update_weights)
+
+        with tf.name_scope('compute_weights'):
+            kernel = tf.nn.l2_normalize(self.v, axis=self.kernel_norm_axes) * g
+
+            if self.is_rnn:
+                self.layer.cell.recurrent_kernel = kernel
+                update_kernel = tf.identity(self.layer.cell.recurrent_kernel)
+            else:
+                self.layer.kernel = kernel
+                update_kernel = tf.identity(self.layer.kernel)
+
+            with tf.control_dependencies([update_kernel]):
+                outputs = self.layer(inputs)
+                return outputs
+
+    def _initialize_weights(self, inputs):
+        """
+        Initialize the weights of the wrapped layer.
+
+        Parameters
+        ----------
+        inputs : tensor
+            Input tensor for data-dependent initialization.
+
+        Returns
+        -------
+        list of tensors
+            A list of tensors for weight initialization.
+        """
+
+        dependencies = [tf.debugging.assert_equal(self._initialized, False, message='The layer has been initialized.')]
+
+        with tf.control_dependencies(dependencies):
+            if self.data_init:
+                with tf.name_scope('data_dep_init'):
+                    x_init = self._naked_clone_layer(inputs)
+                    data_norm_axes = list(range(x_init.shape.rank - 1))
+                    m_init, v_init = tf.nn.moments(x_init, data_norm_axes)
+                    scale_init = 1.0 / tf.math.sqrt(v_init + 1e-10)
+
+                    if scale_init.shape[0] != self.g.shape[0]:
+                        rep = int(self.g.shape[0] / scale_init.shape[0])
+                        scale_init = tf.tile(scale_init, [rep])
+
+                    g_tensor = self.g.assign(self.g * scale_init)
+
+                    if hasattr(self.layer, 'bias') and self.layer.bias is not None:
+                        bias_tensor = self.layer.bias.assign(-m_init * scale_init)
+                        assign_tensors = [g_tensor, bias_tensor]
+                    else:
+                        assign_tensors = [g_tensor]
+            else:
+                with tf.name_scope('init_norm'):
+                    v_flat = tf.reshape(self.v, [-1, self.layer_depth])
+                    v_norm = tf.linalg.norm(v_flat, axis=0)
+                    g_tensor = self.g.assign(tf.reshape(v_norm, (self.layer_depth,)))
+                    assign_tensors = [g_tensor]
+
+            assign_tensors.append(self._initialized.assign(True))
+
+            return assign_tensors
+
+    def compute_output_shape(self, input_shape):
+        """
+        Compute the output shape of the wrapped layer.
+
+        Parameters
+        ----------
+        input_shape : tuple or list
+            The shape of the input to the layer.
+
+        Returns
+        -------
+        tf.TensorShape
+            The computed shape of the output from the layer.
+        """
+
+        if not self.built:
+            self.build(input_shape)
+
+        return tf.TensorShape(self.layer.compute_output_shape(input_shape).as_list())
