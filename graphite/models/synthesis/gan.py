ea20da61c5177291f23821b1826b618f9dceeefc
Arthur Flor <arthurflor23@gmail.com>
2024-01-14 18:20:49 -0300

    Add mask support Fix issues

--- graphite/models/synthesis/gan.py
+++ graphite/models/synthesis/gan.py
@@ -136,7 +136,12 @@ class SynthesisModel(BaseSynthesisModel):
         (aug_image_data, aug_text_data), (image_data, text_data, writer_data) = input_data
 
         batch_size = tf.shape(image_data)[0]
+
+        image_lens = self.get_lens(image_data, pad_value=1)
+        aug_image_lens = self.get_lens(aug_image_data, pad_value=1)
+
         latent_dim = self.style_encoder.latent_dim
+        reduce_scale = self.style_backbone.reduce_scale
 
         self.discriminator.trainable = True
         self.patch_discriminator.trainable = True
@@ -148,6 +153,8 @@ class SynthesisModel(BaseSynthesisModel):
 
         for _ in range(self.discriminator_steps):
             real_features_data, _ = self.style_backbone(image_data, training=True)
+            real_features_data = self.set_mask(real_features_data, image_lens, reduce_scale)
+
             real_latent_data, _, _ = self.style_encoder(real_features_data, training=True)
 
             real_s_real_t_images = self.generator([real_latent_data, text_data], training=True)
@@ -200,6 +207,8 @@ class SynthesisModel(BaseSynthesisModel):
             # writer identifier
             with tf.GradientTape() as tape:
                 wid_features_data, _ = self.style_backbone(aug_image_data, training=True)
+                wid_features_data = self.set_mask(wid_features_data, aug_image_lens, reduce_scale, True)
+
                 wid_logits = self.identification(wid_features_data, training=True)
                 d_wid_loss = self.cls_loss(writer_data, wid_logits)
 
@@ -236,7 +245,16 @@ class SynthesisModel(BaseSynthesisModel):
         (_, aug_text_data), (image_data, text_data, writer_data) = input_data
 
         batch_size = tf.shape(image_data)[0]
+
+        text_lens = self.get_lens(text_data, pad_value=0)
+        image_lens = self.get_lens(image_data, pad_value=1)
+        aug_text_lens = self.get_lens(aug_text_data, pad_value=0)
+
+        aug_image_lens = aug_text_lens * tf.math.floordiv(image_lens, text_lens)
+        aug_image_lens = tf.minimum(tf.shape(image_data)[1], aug_image_lens)
+
         latent_dim = self.style_encoder.latent_dim
+        reduce_scale = self.style_backbone.reduce_scale
 
         self.discriminator.trainable = False
         self.patch_discriminator.trainable = False
@@ -248,6 +266,8 @@ class SynthesisModel(BaseSynthesisModel):
 
         with tf.GradientTape() as tape:
             real_features_data, real_image_feats = self.style_backbone(image_data, training=True)
+            real_features_data = self.set_mask(real_features_data, image_lens, reduce_scale)
+
             real_latent_data, mu, logvar = self.style_encoder(real_features_data, training=True)
 
             real_s_real_t_images = self.generator([real_latent_data, text_data], training=True)
@@ -291,6 +311,8 @@ class SynthesisModel(BaseSynthesisModel):
 
             # style reconstruction
             fake_features_data, _ = self.style_backbone(fake_s_fake_t_images, training=True)
+            fake_features_data = self.set_mask(fake_features_data, aug_image_lens, reduce_scale)
+
             fake_latent_data, _, _ = self.style_encoder(fake_features_data, training=True)
 
             g_sty_loss = tf.reduce_mean(tf.math.abs(fake_latent_data - random_latent_data))
@@ -300,9 +322,13 @@ class SynthesisModel(BaseSynthesisModel):
 
             # writer identifier
             real_t_features_data, real_t_image_feats = self.style_backbone(real_s_real_t_images, training=True)
+            real_t_features_data = self.set_mask(real_t_features_data, image_lens, reduce_scale, True)
+
             real_t_wid_logits = self.identification(real_t_features_data, training=True)
 
             fake_t_features_data, fake_t_image_feats = self.style_backbone(real_s_fake_t_images, training=True)
+            fake_t_features_data = self.set_mask(fake_t_features_data, image_lens, reduce_scale, True)
+
             fake_t_wid_logits = self.identification(fake_t_features_data, training=True)
 
             real_fake_t_wid_logits = tf.concat([real_t_wid_logits, fake_t_wid_logits], axis=0)
@@ -360,7 +386,11 @@ class SynthesisModel(BaseSynthesisModel):
         # kid metric
         _, (image_data, text_data, _) = input_data
 
+        image_lens = self.get_lens(image_data, pad_value=1)
+
         features_data, _ = self.style_backbone(image_data, training=False)
+        features_data = self.set_mask(features_data, image_lens, self.style_backbone.reduce_scale)
+
         latent_data, _, _ = self.style_encoder(features_data, training=False)
         generated_images = self.generator([latent_data, text_data], training=False)
 
@@ -624,7 +654,7 @@ class DiscriminatorModel(tf.keras.Model):
             block = residual_block_down(block, filters, preactive=(i > 0), downsample=(i < len(self.blocks) - 1))
 
         outputs = tf.keras.layers.LeakyReLU(alpha=0.2)(block)
-        outputs = tf.keras.layers.Lambda(lambda x: tf.reduce_sum(x, axis=[1, 2]), name='reduce')(outputs)
+        outputs = tf.keras.layers.Flatten()(outputs)
         outputs = SpectralNormalization(tf.keras.layers.Dense(units=1))(outputs)
 
         self.model = tf.keras.Model(inputs=image_inputs, outputs=outputs, name=self.name)
@@ -843,6 +873,7 @@ class StyleEncoderModel(tf.keras.Model):
             lambda x: x[0] + tf.exp(0.5 * x[1]) * tf.random.normal(tf.shape(x[1])), name='reparam')([mu, logvar])
 
         self.features_output_shape = outputs.get_shape()[1:]
+
         self.model = tf.keras.Model(inputs=feature_inputs, outputs=[outputs, mu, logvar], name=self.name)
 
 
@@ -910,11 +941,9 @@ class IdentificationModel(tf.keras.Model):
             and configurations. It is typically called in the constructor to create the model structure.
         """
 
-        feature_inputs = tf.keras.layers.Input(shape=self.features_shape)
+        feature_inputs = tf.keras.layers.Input(shape=self.features_shape[:-1])
 
-        style = tf.keras.layers.Lambda(lambda x: tf.reduce_mean(x, axis=-2), name='reduce')(feature_inputs)
-
-        style_dense = tf.keras.layers.Dense(self.features_shape[-1])(style)
+        style_dense = tf.keras.layers.Dense(self.features_shape[-1])(feature_inputs)
         style_dense = tf.keras.layers.LeakyReLU(alpha=0.2)(style_dense)
 
         outputs = tf.keras.layers.Dense(self.writers_shape[0])(style_dense)
