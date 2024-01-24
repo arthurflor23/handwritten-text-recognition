26179a225df4a2969d04d33aad6e3ca64522b819
Arthur Flor <arthurflor23@gmail.com>
2024-01-24 14:40:55 -0300

    Update losses

--- graphite/models/components/losses.py
+++ graphite/models/components/losses.py
@@ -53,22 +53,22 @@ class CTCLoss(tf.keras.losses.Loss):
         logits = tf.transpose(tf.math.log(y_pred + self.epsilon), perm=[1, 0, 2])
 
         logit_length = tf.reduce_sum(tf.reduce_sum(y_pred, axis=-1), axis=-1)
-        # label_length = tf.math.count_nonzero(y_true, axis=-1)
+        label_length = tf.math.count_nonzero(y_true, axis=-1)
 
-        # ctc_loss = tf.nn.ctc_loss(labels=tf.cast(labels, dtype=tf.int32),
-        #                           logits=tf.cast(logits, dtype=tf.float32),
-        #                           label_length=tf.cast(label_length, dtype=tf.int32),
-        #                           logit_length=tf.cast(logit_length, dtype=tf.int32),
-        #                           logits_time_major=True,
-        #                           blank_index=0)
+        ctc_loss = tf.nn.ctc_loss(labels=tf.cast(labels, dtype=tf.int32),
+                                  logits=tf.cast(logits, dtype=tf.float32),
+                                  label_length=tf.cast(label_length, dtype=tf.int32),
+                                  logit_length=tf.cast(logit_length, dtype=tf.int32),
+                                  logits_time_major=True,
+                                  blank_index=0)
 
-        ctc_loss = tf.compat.v1.nn.ctc_loss(labels=tf.cast(labels, dtype=tf.int32),
-                                            inputs=tf.cast(logits, dtype=tf.float32),
-                                            sequence_length=tf.cast(logit_length, dtype=tf.int32),
-                                            preprocess_collapse_repeated=False,
-                                            ctc_merge_repeated=True,
-                                            ignore_longer_outputs_than_inputs=True,
-                                            time_major=True)
+        # ctc_loss = tf.compat.v1.nn.ctc_loss(labels=tf.cast(labels, dtype=tf.int32),
+        #                                     inputs=tf.cast(logits, dtype=tf.float32),
+        #                                     sequence_length=tf.cast(logit_length, dtype=tf.int32),
+        #                                     preprocess_collapse_repeated=False,
+        #                                     ctc_merge_repeated=True,
+        #                                     ignore_longer_outputs_than_inputs=True,
+        #                                     time_major=True)
 
         ctc_loss = tf.reduce_mean(ctc_loss)
 
