c1154a32fafce332a37f16369d8485ba1c76361b
Arthur Flor <arthurflor23@gmail.com>
2024-01-26 09:23:40 -0300

    Add backbone flag support

--- graphite/models/recognition/bluche.py
+++ graphite/models/recognition/bluche.py
@@ -35,12 +35,17 @@ class RecognitionModel(BaseRecognitionModel):
         self.optimizer = NormalizedOptimizer(
             tf.keras.optimizers.RMSprop(learning_rate=learning_rate))
 
-    def build_model(self):
+    def build_model(self, backbone=False):
         """
         Builds the neural network model.
 
         This method sets up the architecture of the model by defining layers, their connections,
             and configurations. It is typically called in the constructor to create the model structure.
+
+        Parameters
+        ----------
+        backbone : bool, optional
+            Flag to set the backbone model.
         """
 
         image_inputs = tf.keras.Input(shape=self.image_shape)
@@ -87,6 +92,9 @@ class RecognitionModel(BaseRecognitionModel):
         conv = tf.keras.layers.MaxPooling2D(pool_size=(1, 4), strides=(1, 4), padding='valid')(conv)
         conv = tf.keras.layers.Reshape(target_shape=(conv.get_shape()[1], -1))(conv)
         # conv = MaskingPadding()([image_inputs, conv])
+
+        if backbone:
+            self.backbone = tf.keras.Model(inputs=image_inputs, outputs=conv, name='backbone')
 
         blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, return_sequences=True))(conv)
         blstm = tf.keras.layers.Dense(units=128, activation='tanh')(blstm)
