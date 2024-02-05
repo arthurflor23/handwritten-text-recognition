876703ff392e80a7be3ad9d56106027572c6657e
Arthur Flor <arthurflor23@gmail.com>
2024-02-05 10:05:43 -0300

    Update default model - Add encoder and decoder support

--- graphite/models/recognition/bluche.py
+++ graphite/models/recognition/bluche.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 
 from graphite.models.components.common import BaseRecognitionModel
 from graphite.models.components.layers import GatedConv2D
-from graphite.models.components.optimizers import NormalizedOptimizer
 
 
 class RecognitionModel(BaseRecognitionModel):
@@ -31,75 +30,77 @@ class RecognitionModel(BaseRecognitionModel):
         if learning_rate is None:
             learning_rate = 4e-4
 
-        self.optimizer = NormalizedOptimizer(
-            tf.keras.optimizers.RMSprop(learning_rate=learning_rate))
+        self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
 
-    def build_model(self, backbone=False):
+    def build_model(self):
         """
         Builds the neural network model.
 
         This method sets up the architecture of the model by defining layers, their connections,
             and configurations. It is typically called in the constructor to create the model structure.
-
-        Parameters
-        ----------
-        backbone : bool, optional
-            Flag to set the backbone model.
         """
 
-        image_inputs = tf.keras.Input(shape=self.image_shape)
+        # encoder model
+        encoder_input = tf.keras.Input(shape=self.image_shape)
 
-        conv = tf.keras.layers.Reshape(target_shape=(512, 64, -1))(image_inputs)
+        encoder = tf.keras.layers.Reshape(target_shape=(512, 64, -1))(encoder_input)
 
-        conv = tf.keras.layers.Conv2D(filters=8,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same',
-                                      activation='tanh')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=8,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same',
+                                         activation='tanh')(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=16,
-                                      kernel_size=(2, 4),
-                                      strides=(2, 4),
-                                      padding='same',
-                                      activation='tanh')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=16,
+                                         kernel_size=(2, 4),
+                                         strides=(2, 4),
+                                         padding='same',
+                                         activation='tanh')(encoder)
 
-        conv = GatedConv2D(filters=16, fullgate=False)(conv)
+        encoder = GatedConv2D(filters=16, fullgate=False)(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=32,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same',
-                                      activation='tanh')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=32,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same',
+                                         activation='tanh')(encoder)
 
-        conv = GatedConv2D(filters=32, fullgate=False)(conv)
+        encoder = GatedConv2D(filters=32, fullgate=False)(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=64,
-                                      kernel_size=(2, 4),
-                                      strides=(1, 4),
-                                      padding='same',
-                                      activation='tanh')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=64,
+                                         kernel_size=(2, 4),
+                                         strides=(1, 4),
+                                         padding='same',
+                                         activation='tanh')(encoder)
 
-        conv = GatedConv2D(filters=64, fullgate=False)(conv)
+        encoder = GatedConv2D(filters=64, fullgate=False)(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=128,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same',
-                                      activation='tanh')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=128,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same',
+                                         activation='tanh')(encoder)
 
-        conv = tf.keras.layers.MaxPooling2D(pool_size=(1, 4), strides=(1, 4), padding='valid')(conv)
+        encoder = tf.keras.layers.MaxPooling2D(pool_size=(1, 4), strides=(1, 4), padding='valid')(encoder)
+        encoder = tf.keras.layers.Reshape(target_shape=(encoder.shape[1], -1))(encoder)
 
-        conv = tf.keras.layers.Reshape(target_shape=(conv.get_shape()[1], -1))(conv)
+        self.encoder = tf.keras.Model(inputs=encoder_input, outputs=encoder, name='encoder')
 
-        if backbone:
-            self.backbone = tf.keras.Model(inputs=image_inputs, outputs=conv, name='backbone')
+        # decoder model
+        decoder_input = tf.keras.Input(shape=encoder.shape[1:])
 
-        blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, return_sequences=True))(conv)
-        blstm = tf.keras.layers.Dense(units=128, activation='tanh')(blstm)
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(units=128, return_sequences=True))(decoder_input)
+        decoder = tf.keras.layers.Dense(units=128, activation='tanh')(decoder)
 
-        blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, return_sequences=True))(blstm)
-        blstm = tf.keras.layers.Dense(units=self.lexical_shape[-1], activation='softmax')(blstm)
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(units=128, return_sequences=True))(decoder)
+        decoder = tf.keras.layers.Dense(units=self.lexical_shape[-1], activation='softmax')(decoder)
 
-        outputs = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-2), name='expand_dims')(blstm)
+        decoder = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-2), name='expand_dims')(decoder)
 
-        self.recognition = tf.keras.Model(inputs=image_inputs, outputs=outputs, name=self.name)
+        self.decoder = tf.keras.Model(inputs=decoder_input, outputs=decoder, name='decoder')
+
+        # recognition model
+        decoder_output = self.decoder(self.encoder.output)
+        self.recognition = tf.keras.Model(inputs=encoder_input, outputs=decoder_output, name=self.name)
