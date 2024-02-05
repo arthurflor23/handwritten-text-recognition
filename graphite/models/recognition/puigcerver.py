d64f68622320e532d294239c3784ea0e7f203652
Arthur Flor <arthurflor23@gmail.com>
2024-02-05 10:05:52 -0300

    Update default model - Add encoder and decoder support

--- graphite/models/recognition/puigcerver.py
+++ graphite/models/recognition/puigcerver.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
 from graphite.models.components.common import BaseRecognitionModel
-from graphite.models.components.optimizers import NormalizedOptimizer
 
 
 class RecognitionModel(BaseRecognitionModel):
@@ -30,84 +29,94 @@ class RecognitionModel(BaseRecognitionModel):
         if learning_rate is None:
             learning_rate = 3e-4
 
-        self.optimizer = NormalizedOptimizer(
-            tf.keras.optimizers.RMSprop(learning_rate=learning_rate))
+        self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
 
-    def build_model(self, backbone=False):
+    def build_model(self):
         """
         Builds the neural network model.
 
         This method sets up the architecture of the model by defining layers, their connections,
             and configurations. It is typically called in the constructor to create the model structure.
-
-        Parameters
-        ----------
-        backbone : bool, optional
-            Flag to set the backbone model.
         """
 
-        image_inputs = tf.keras.Input(shape=self.image_shape)
+        # encoder model
+        encoder_input = tf.keras.Input(shape=self.image_shape)
 
-        conv = tf.keras.layers.Conv2D(filters=16,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same')(image_inputs)
+        encoder = tf.keras.layers.Conv2D(filters=16,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same')(encoder_input)
 
-        conv = tf.keras.layers.BatchNormalization(renorm=True)(conv)
-        conv = tf.keras.layers.LeakyReLU(alpha=0.01)(conv)
-        conv = tf.keras.layers.MaxPooling2D(pool_size=(2, 1), strides=(2, 1), padding='valid')(conv)
+        encoder = tf.keras.layers.BatchNormalization()(encoder)
+        encoder = tf.keras.layers.LeakyReLU(alpha=0.01)(encoder)
+        encoder = tf.keras.layers.MaxPooling2D(pool_size=(2, 1), strides=(2, 1), padding='valid')(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=32,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=32,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same')(encoder)
 
-        conv = tf.keras.layers.BatchNormalization(renorm=True)(conv)
-        conv = tf.keras.layers.LeakyReLU(alpha=0.01)(conv)
-        conv = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(conv)
-        conv = tf.keras.layers.Dropout(rate=0.2)(conv)
+        encoder = tf.keras.layers.BatchNormalization()(encoder)
+        encoder = tf.keras.layers.LeakyReLU(alpha=0.01)(encoder)
+        encoder = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(encoder)
+        encoder = tf.keras.layers.Dropout(rate=0.2)(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=48,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=48,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same')(encoder)
 
-        conv = tf.keras.layers.BatchNormalization(renorm=True)(conv)
-        conv = tf.keras.layers.LeakyReLU(alpha=0.01)(conv)
-        conv = tf.keras.layers.MaxPooling2D(pool_size=(1, 2), strides=(1, 2), padding='valid')(conv)
-        conv = tf.keras.layers.Dropout(rate=0.2)(conv)
+        encoder = tf.keras.layers.BatchNormalization()(encoder)
+        encoder = tf.keras.layers.LeakyReLU(alpha=0.01)(encoder)
+        encoder = tf.keras.layers.MaxPooling2D(pool_size=(1, 2), strides=(1, 2), padding='valid')(encoder)
+        encoder = tf.keras.layers.Dropout(rate=0.2)(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=64,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=64,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same')(encoder)
 
-        conv = tf.keras.layers.BatchNormalization(renorm=True)(conv)
-        conv = tf.keras.layers.LeakyReLU(alpha=0.01)(conv)
-        conv = tf.keras.layers.Dropout(rate=0.2)(conv)
+        encoder = tf.keras.layers.BatchNormalization()(encoder)
+        encoder = tf.keras.layers.LeakyReLU(alpha=0.01)(encoder)
+        encoder = tf.keras.layers.Dropout(rate=0.2)(encoder)
 
-        conv = tf.keras.layers.Conv2D(filters=80,
-                                      kernel_size=(3, 3),
-                                      strides=(1, 1),
-                                      padding='same')(conv)
+        encoder = tf.keras.layers.Conv2D(filters=80,
+                                         kernel_size=(3, 3),
+                                         strides=(1, 1),
+                                         padding='same')(encoder)
 
-        conv = tf.keras.layers.BatchNormalization(renorm=True)(conv)
-        conv = tf.keras.layers.LeakyReLU(alpha=0.01)(conv)
+        encoder = tf.keras.layers.BatchNormalization()(encoder)
+        encoder = tf.keras.layers.LeakyReLU(alpha=0.01)(encoder)
 
-        conv = tf.keras.layers.Reshape(target_shape=(conv.get_shape()[1], -1))(conv)
+        encoder = tf.keras.layers.Reshape(target_shape=(encoder.shape[1], -1))(encoder)
 
-        if backbone:
-            self.backbone = tf.keras.Model(inputs=image_inputs, outputs=conv, name='backbone')
+        self.encoder = tf.keras.Model(inputs=encoder_input, outputs=encoder, name='encoder')
 
-        blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(conv)
-        blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(blstm)
-        blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(blstm)
-        blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(blstm)
-        blstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(blstm)
+        # decoder model
+        decoder_input = tf.keras.Input(shape=encoder.shape[1:])
 
-        blstm = tf.keras.layers.Dropout(rate=0.5)(blstm)
-        blstm = tf.keras.layers.Dense(units=self.lexical_shape[-1], activation='softmax')(blstm)
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(decoder_input)
 
-        outputs = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-2), name='expand_dims')(blstm)
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(decoder)
 
-        self.recognition = tf.keras.Model(inputs=image_inputs, outputs=outputs, name=self.name)
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(decoder)
+
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(decoder)
+
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(256, return_sequences=True, dropout=0.5))(decoder)
+
+        decoder = tf.keras.layers.Dropout(rate=0.5)(decoder)
+        decoder = tf.keras.layers.Dense(units=self.lexical_shape[-1], activation='softmax')(decoder)
+
+        decoder = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-2), name='expand_dims')(decoder)
+
+        self.decoder = tf.keras.Model(inputs=decoder_input, outputs=decoder, name='decoder')
+
+        # recognition model
+        decoder_output = self.decoder(self.encoder.output)
+        self.recognition = tf.keras.Model(inputs=encoder_input, outputs=decoder_output, name=self.name)
