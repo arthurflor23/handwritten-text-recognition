c5271c7fd7cef0c39ca661fad3fe3b2e05257a90
Arthur Flor <arthurflor23@gmail.com>
2024-02-07 09:35:37 -0300

    Update model

--- graphite/models/recognition/flor_tcn.py
+++ graphite/models/recognition/flor_tcn.py
@@ -4,7 +4,6 @@ from graphite.models.components.common import BaseRecognitionModel
 from graphite.models.components.layers import GatedConv2D
 from graphite.models.components.layers import MaskPadding
 from graphite.models.components.layers import SelfAttention
-from graphite.models.components.layers import TemporalConvolutional
 from graphite.models.components.optimizers import NormalizedOptimizer
 
 
@@ -26,9 +25,6 @@ class RecognitionModel(BaseRecognitionModel):
 
     HTR-Flor: A Deep Learning System for Offline Handwritten Text Recognition
         https://ieeexplore.ieee.org/document/9266005
-
-    Temporal Convolutional Networks for Action Segmentation and Detection
-        https://arxiv.org/abs/1611.05267
     """
 
     def compile(self, learning_rate=None):
@@ -131,30 +127,34 @@ class RecognitionModel(BaseRecognitionModel):
         encoder = SelfAttention()(encoder)
 
         encoder = MaskPadding()([encoder_input, encoder])
+
         encoder = tf.keras.layers.Reshape(target_shape=(encoder.shape[1], -1))(encoder)
-        encoder = tf.keras.layers.Dense(units=256)(encoder)
+        encoder = tf.keras.layers.Dense(units=256, kernel_initializer='he_uniform')(encoder)
+
+        encoder = tf.keras.layers.PReLU(shared_axes=[-1])(encoder)
+        encoder = tf.keras.layers.BatchNormalization(renorm=True)(encoder)
 
         self.encoder = tf.keras.Model(name='encoder', inputs=encoder_input, outputs=encoder)
 
         # decoder model
         decoder_input = tf.keras.Input(shape=encoder.shape[1:])
 
-        decoder = TemporalConvolutional(filters=64,
-                                        nb_stacks=3,
-                                        kernel_size=3,
-                                        dilations=(1, 2, 4, 8),
-                                        padding='same',
-                                        activation='prelu',
-                                        batch_norm=True,
-                                        return_sequences=True,
-                                        dropout=0.2)(decoder_input)
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.GRU(units=128, dropout=0.5, return_sequences=True))(decoder_input)
+
+        decoder = tf.keras.layers.Dense(units=256, activation='tanh')(decoder)
+
+        decoder = tf.keras.layers.Bidirectional(
+            tf.keras.layers.GRU(units=128, dropout=0.5, return_sequences=True))(decoder)
+
+        # decoder = tf.keras.layers.Dropout(rate=0.5)(decoder)
 
         decoder = tf.keras.layers.Dense(units=self.lexical_shape[-1], activation='softmax')(decoder)
-
         decoder = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-2), name='expand_dims')(decoder)
 
         self.decoder = tf.keras.Model(name='decoder', inputs=decoder_input, outputs=decoder)
 
         # recognition model
-        decoder_output = self.decoder(self.encoder.output)
-        self.recognition = tf.keras.Model(name=self.name, inputs=encoder_input, outputs=decoder_output)
+        self.recognition = tf.keras.Model(name=self.name,
+                                          inputs=self.encoder.input,
+                                          outputs=self.decoder(self.encoder.output))
