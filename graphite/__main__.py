836a5c3dd2647c6c9fe35ec1ea7e3d1930835356
Arthur Flor <arthurflor23@gmail.com>
2024-02-09 00:06:32 -0300

    Update default values

--- graphite/__main__.py
+++ graphite/__main__.py
@@ -62,12 +62,12 @@ if __name__ == '__main__':
     # training
     parser.add_argument('--training', default=False, action='store_true', help='Perform training pipeline')
     parser.add_argument('--epochs', default=None, type=int, help='Maximum number of epochs')
-    parser.add_argument('--batch-size', default=8, type=int, help='Batch size')
+    parser.add_argument('--batch-size', default=16, type=int, help='Batch size')
     parser.add_argument('--learning-rate', default=None, type=float, help='Optimizer learning rate')
     parser.add_argument('--plateau-factor', default=0.1, type=float, help='Learning rate reduction factor')
     parser.add_argument('--plateau-cooldown', default=0, type=int, help='Cooldown after rate plateau')
     parser.add_argument('--plateau-patience', default=20, type=int, help='Epochs before recognizing a plateau')
-    parser.add_argument('--patience', default=40, type=int, help='Epochs without improvement to stop')
+    parser.add_argument('--patience', default=30, type=int, help='Epochs without improvement to stop')
 
     # test
     parser.add_argument('--test', default=False, action='store_true', help='Perform test pipeline')
