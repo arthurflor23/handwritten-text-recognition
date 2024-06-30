73b6d9fd81da1857a7e82a0a0b0de4d530b2f69c
Arthur Flor <arthurflor23@gmail.com>
2024-06-30 12:04:08 -0300

    Update main script

--- sarah/__main__.py
+++ sarah/__main__.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('--binarize', default=None, nargs='+', help='Binarization settings')
     parser.add_argument('--erode', default=None, nargs=2, type=float, help='Erosion settings')
     parser.add_argument('--dilate', default=None, nargs=2, type=float, help='Dilation settings')
-    parser.add_argument('--elastic', default=[0.33, 29], nargs=2, type=float, help='Elastic deformation settings')
+    parser.add_argument('--elastic', default=[0.66, 29], nargs=2, type=float, help='Elastic deformation settings')
     parser.add_argument('--perspective', default=[0.66, 0.4], nargs=2, type=float, help='Perspective settings')
     parser.add_argument('--mixup', default=None, nargs=2, type=float, help='Mixup settings')
     parser.add_argument('--shear', default=[0.33, 15], nargs=2, type=float, help='Shearing settings')
