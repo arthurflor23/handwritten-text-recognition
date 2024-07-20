7c68850fc34c0368bb6be9382746def2a3ef129c
Arthur Flor <arthurflor23@gmail.com>
2024-07-20 09:01:50 -0300

    Update optical model

--- sarah/models/recognition/flor_v2.py
+++ sarah/models/recognition/flor_v2.py
@@ -72,7 +72,8 @@ class RecognitionModel(BaseRecognitionModel):
         encoder = tf.keras.layers.PReLU(shared_axes=[1, 2])(encoder)
         encoder = tf.keras.layers.MaxPooling2D(pool_size=2, strides=2)(encoder)
 
-        encoder = GatedConv2D(mode='residual')(encoder)
+        encoder = tf.keras.layers.Dropout(rate=0.2)(encoder)
+        encoder = SelfAttention()(encoder)
 
         encoder = tf.keras.layers.Conv2D(filters=96, kernel_size=3, padding='same')(encoder)
         encoder = tf.keras.layers.BatchNormalization()(encoder)
@@ -96,7 +97,7 @@ class RecognitionModel(BaseRecognitionModel):
         encoder = tf.keras.layers.MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(encoder)
 
         encoder = tf.keras.layers.Dropout(rate=0.2)(encoder)
-        encoder = SelfAttention()(encoder)
+        encoder = SelfAttention(pooling=False)(encoder)
 
         self.encoder = tf.keras.Model(name='encoder', inputs=encoder_input, outputs=encoder)
 
