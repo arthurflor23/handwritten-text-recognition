f568e6aa61320935a817bb9c99d6922a622bea50
Arthur Flor <arthurflor23@gmail.com>
2024-09-02 13:02:11 -0300

    Update compose logs

--- sarah/models/compose.py
+++ sarah/models/compose.py
@@ -341,9 +341,7 @@ class Compose():
 
         if monitor in history.history:
             best_metric_index = history.history[monitor].index(min(history.history[monitor]))
-
-            metrics = {k: history.history[k][best_metric_index]
-                       for k in history.history if k not in ('lr', 'learning_rate')}
+            metrics = {k: history.history[k][best_metric_index] for k in history.history}
 
             training_metrics = {k: metrics[k] for k in metrics if k[:4] != 'val_'}
             self.save_context(metrics=training_metrics, prefix='training')
