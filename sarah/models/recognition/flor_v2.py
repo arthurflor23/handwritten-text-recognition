4c31a1740e3cde340bd39983414967a5afdf5c05
Arthur Flor <arthurflor23@gmail.com>
2024-09-09 13:40:55 -0300

    Update optical model

--- sarah/models/recognition/flor_v2.py
+++ sarah/models/recognition/flor_v2.py
@@ -76,7 +76,7 @@ class RecognitionModel(BaseRecognitionModel):
         encoder = GatedConv2D(mode='residual', dropout=0.1)(encoder)
         encoder = tf.keras.layers.Dropout(rate=0.1)(encoder)
 
-        encoder = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same')(encoder)
+        encoder = tf.keras.layers.Conv2D(filters=72, kernel_size=3, padding='same')(encoder)
         encoder = tf.keras.layers.BatchNormalization()(encoder)
         encoder = tf.keras.layers.Activation(activation='swish')(encoder)
         encoder = tf.keras.layers.MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(encoder)
@@ -84,18 +84,10 @@ class RecognitionModel(BaseRecognitionModel):
         encoder = GatedConv2D(mode='residual', dropout=0.1)(encoder)
         encoder = tf.keras.layers.Dropout(rate=0.1)(encoder)
 
-        encoder = tf.keras.layers.Conv2D(filters=80, kernel_size=3, padding='same')(encoder)
-        encoder = tf.keras.layers.BatchNormalization()(encoder)
-        encoder = tf.keras.layers.Activation(activation='swish')(encoder)
-        encoder = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(encoder)
-
-        encoder = SelfAttention(dropout=0.1)(encoder)
-        encoder = tf.keras.layers.Dropout(rate=0.1)(encoder)
-
         encoder = tf.keras.layers.Conv2D(filters=96, kernel_size=3, padding='same')(encoder)
         encoder = tf.keras.layers.BatchNormalization()(encoder)
         encoder = tf.keras.layers.Activation(activation='swish')(encoder)
-        encoder = tf.keras.layers.MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(encoder)
+        encoder = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(encoder)
 
         encoder = SelfAttention(dropout=0.1)(encoder)
         encoder = tf.keras.layers.Dropout(rate=0.1)(encoder)
@@ -111,6 +103,7 @@ class RecognitionModel(BaseRecognitionModel):
         encoder = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='same')(encoder)
         encoder = tf.keras.layers.BatchNormalization()(encoder)
         encoder = tf.keras.layers.Activation(activation='swish')(encoder)
+        encoder = tf.keras.layers.MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(encoder)
 
         encoder = SelfAttention()(encoder)
 
