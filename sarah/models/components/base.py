5b43b3011281c3d51d594505aa2870ac8aa4af6b
Arthur Flor <arthurflor23@gmail.com>
2024-11-21 08:54:18 -0300

    Update base class

--- sarah/models/components/base.py
+++ sarah/models/components/base.py
@@ -622,7 +622,7 @@ class BaseSynthesisModel(BaseModel):
         self.cls_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         self.ctc_loss = CTCLoss()
         self.ctx_loss = CTXLoss()
-        self.kid = KernelInceptionDistance(scale=127.5, offset=127.5)
+        self.kid = KernelInceptionDistance(image_shape=self.image_shape)
         self.kld_loss = KLDivergence(non_cyclical_beta=1e-4)
 
         self.measure_tracker = MeasureTracker()
