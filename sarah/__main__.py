45388002dd3c6d566ab271b05405865c7c50eef4
Arthur Flor <arthurflor23@gmail.com>
2025-01-22 10:10:15 -0300

    Update main script

--- sarah/__main__.py
+++ sarah/__main__.py
@@ -68,6 +68,7 @@ if __name__ == '__main__':
 
     # training
     parser.add_argument('--training', default=False, action='store_true', help='Perform training pipeline')
+    parser.add_argument('--training-step-factor', default=1, type=int, help='Factor for training steps')
     parser.add_argument('--epochs', default=None, type=int, help='Maximum number of epochs')
     parser.add_argument('--batch-size', default=8, type=int, help='Batch size')
     parser.add_argument('--learning-rate', default=None, type=float, help='Learning rate')
