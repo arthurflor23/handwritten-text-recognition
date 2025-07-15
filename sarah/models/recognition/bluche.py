65c702aa917707b65ae1cbde70485d731e16d787
Arthur Flor <arthurflor23@gmail.com>
2025-07-15 12:54:11 -0300

    Update optical model

--- sarah/models/recognition/bluche.py
+++ sarah/models/recognition/bluche.py
@@ -81,7 +81,7 @@ class RecognitionModel(BaseRecognitionModel):
 
         encoder = tf.keras.layers.MaxPooling2D(pool_size=(1, 4), strides=(1, 4), padding='valid')(encoder)
 
-        self.encoder = tf.keras.Model(name=f"{self.name}_encoder", inputs=encoder_input, outputs=encoder)
+        self.encoder = tf.keras.Model(name='recognition_encoder', inputs=encoder_input, outputs=encoder)
 
         # decoder model
         decoder_input = tf.keras.Input(shape=encoder.shape[1:])
@@ -102,7 +102,7 @@ class RecognitionModel(BaseRecognitionModel):
         decoder = tf.keras.layers.Reshape(target_shape=encoder.shape[1:-1] + self.lexical_shape[-1:])(decoder)
         decoder = tf.keras.layers.Lambda(lambda x: tf.transpose(x, perm=(0, 2, 1, 3)), name='perm2')(decoder)
 
-        self.decoder = tf.keras.Model(name=f"{self.name}_decoder", inputs=decoder_input, outputs=decoder)
+        self.decoder = tf.keras.Model(name='recognition_decoder', inputs=decoder_input, outputs=decoder)
 
         # recognition model
         self.recognition = tf.keras.Model(name=self.name,
