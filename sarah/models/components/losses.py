ee5f04d6dd3979638385c01b036cba5ad0abe6c7
Arthur Flor <arthurflor23@gmail.com>
2025-09-22 21:19:31 -0300

    Update losses

--- sarah/models/components/losses.py
+++ sarah/models/components/losses.py
@@ -155,10 +155,10 @@ class CTXLoss(tf.keras.losses.Loss):
                 distance = tf.reduce_sum(tf.square(diff), axis=-1)
 
         d_min = tf.math.reduce_min(distance, axis=1, keepdims=True)
-        d_tilde = distance / (d_min + self.epsilon)
+        d_tilde = tf.keras.ops.divide_no_nan(distance, d_min)
 
         w = tf.math.exp((1 - d_tilde) / self.sigma)
-        ctx_ij = w / (tf.math.reduce_sum(w, axis=1, keepdims=True) + self.epsilon)
+        ctx_ij = tf.keras.ops.divide_no_nan(w, tf.reduce_sum(w, axis=1, keepdims=True))
 
         ctx = tf.reduce_mean(tf.reduce_max(ctx_ij, axis=1))
         ctx_loss = tf.math.reduce_mean(-tf.math.log(ctx + self.epsilon))
