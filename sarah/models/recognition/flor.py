6d8fd7998ec0c4e0ddd411955771dbbfc5351b78
Arthur Flor <arthurflor23@gmail.com>
2025-12-17 10:15:36 -0300

    Update optical model

--- sarah/models/recognition/flor.py
+++ sarah/models/recognition/flor.py
@@ -98,7 +98,7 @@ class RecognitionModel(BaseRecognitionModel):
         # decoder model
         decoder_input = tf.keras.Input(shape=encoder.shape[1:])
 
-        decoder = SelfAttentionDense(k=1/8, h=1.5)(decoder_input)
+        decoder = SelfAttentionDense(k=1/8, h=1.5, pooling=True)(decoder_input)
         decoder = tf.keras.layers.Reshape(target_shape=(-1, decoder.shape[-1]))(decoder)
 
         for rate in [0.4, 0.6, 0.6]:
