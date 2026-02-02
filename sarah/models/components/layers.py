6c0975d0f7e4829f952b81f1dfe365eec353456d
Arthur Flor <arthurflor23@gmail.com>
2026-02-02 07:13:28 +0100

    Update layers

--- sarah/models/components/layers.py
+++ sarah/models/components/layers.py
@@ -212,7 +212,7 @@ class ContentAlignment(tf.keras.layers.Layer):
                  image_padding_value=-1,
                  text_padding_value=0,
                  mask_padding_value=0,
-                 resize_method='bilinear',
+                 resize_method='bicubic',
                  **kwargs):
         """
         Initializes the layer.
