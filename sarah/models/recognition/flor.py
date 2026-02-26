01924c84c01bbef43d3fb91e8c5e49e0e2d4d2d6
Arthur Flor <arthurflor23@gmail.com>
2026-02-26 16:06:39 +0100

    Update optical model

--- sarah/models/recognition/flor.py
+++ sarah/models/recognition/flor.py
@@ -102,7 +102,7 @@ class RecognitionModel(BaseRecognitionModel):
 
         decoder = tf.keras.layers.Reshape(target_shape=(-1, decoder.shape[-1]))(decoder)
 
-        for rate in [0.3, 0.6, 0.6]:
+        for rate in [0.5, 0.5, 0.5]:
             forwards = tf.keras.layers.Dropout(rate=rate)(decoder)
             forwards = tf.keras.layers.LSTM(units=128, return_sequences=True, go_backwards=False)(forwards)
 
